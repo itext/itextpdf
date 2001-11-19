ea19bc49f0cea7181e226ad2322b33b35a7fd07d
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2001-11-19 11:43:39 +0000

    added comments

--- src/com/lowagie/text/pdf/wmf/MetaFont.java
+++ src/com/lowagie/text/pdf/wmf/MetaFont.java
@@ -1,3 +1,36 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * Copyright 2001 by Paulo Soares.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library general Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Library General Public License along
+ * with this library; if not, write to the Free Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * If you didn't download this code from the following link, you should check if
+ * you aren't using an obsolete version:
+ * http://www.lowagie.com/iText/
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ *
+ */
+
 package com.lowagie.text.pdf.wmf;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -5,11 +38,6 @@ import java.awt.Color;
 import com.lowagie.text.pdf.*;
 import com.lowagie.text.DocumentException;
 
-/**
- *
- * @author  Administrator
- * @version 
- */
 public class MetaFont extends MetaObject {
     static final String fontNames[] = {
         "Courier", "Courier-Bold", "Courier-Oblique", "Courier-BoldOblique",
