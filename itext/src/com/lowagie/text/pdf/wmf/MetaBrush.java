764e40f55487ffe096688e9d642c81a42889e3d9
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2001-11-19 11:43:39 +0000

    added comments
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@370 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/pdf/wmf/MetaBrush.java
+++ itext/src/com/lowagie/text/pdf/wmf/MetaBrush.java
@@ -1,18 +1,40 @@
 /*
- * MetaPen.java
+ * $Id$
+ * $Name$
  *
- * Created on October 28, 2001, 11:20 AM
+ * Copyright 2001 by Paulo Soares.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or any
+ * later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library general Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Library General Public License along
+ * with this library; if not, write to the Free Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA.
+ *
+ * If you didn't download this code from the following link, you should check if
+ * you aren't using an obsolete version:
+ * http://www.lowagie.com/iText/
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ *
  */
 
 package com.lowagie.text.pdf.wmf;
 import java.io.IOException;
 import java.awt.Color;
 
-/**
- *
- * @author  Administrator
- * @version 
- */
 public class MetaBrush extends MetaObject {
 
     public static final int BS_SOLID = 0;
