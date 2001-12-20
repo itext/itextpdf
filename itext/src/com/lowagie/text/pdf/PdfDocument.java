6c30979c314d17216d7844191241fab24897f5ef
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2001-12-20 10:50:34 +0000

    hotfix for Frederic Le Bail
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@495 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/pdf/PdfDocument.java
+++ itext/src/com/lowagie/text/pdf/PdfDocument.java
@@ -1002,21 +1002,25 @@ class PdfDocument extends Document implements DocListener {
                     // we cast the element to a paragraph
                     Paragraph paragraph = (Paragraph) element;
                     
-                    //add by Jin-Hsia Yang
-                    isParagraphE = true;
-                    paraIndent = paragraph.indentationLeft();
-                    //end add by Jin-Hsia Yang
-                    
                     // we adjust the parameters of the document
                     alignment = paragraph.alignment();
-                    indentLeft += paragraph.indentationLeft();
-                    indentRight += paragraph.indentationRight();
                     leading = paragraph.leading();
+                    
                     carriageReturn();
                     // we don't want to make orphans/widows
                     if (currentHeight + line.height() + leading > indentTop() - indentBottom()) {
                         newPage();
                     }
+                    indentLeft += paragraph.indentationLeft();
+                    indentRight += paragraph.indentationRight();
+                    
+                    carriageReturn();
+                    
+                    //add by Jin-Hsia Yang
+                    isParagraphE = true;
+                    paraIndent += paragraph.indentationLeft();
+                    //end add by Jin-Hsia Yang
+                    
                     PdfPageEvent pageEvent = writer.getPageEvent();
                     if (pageEvent != null && isParagraph)
                         pageEvent.onParagraph(writer, this, indentTop() - currentHeight);
@@ -1028,6 +1032,11 @@ class PdfDocument extends Document implements DocListener {
                     //					line.resetAlignment();
                     //				}
                     // some parameters are set back to normal again
+                    
+                    //add by Jin-Hsia Yang and blowagie
+                    paraIndent -= paragraph.indentationLeft();
+                    //end add by Jin-Hsia Yang and blowagie
+                    
                     carriageReturn();
                     
                     if (pageEvent != null && isParagraph)
