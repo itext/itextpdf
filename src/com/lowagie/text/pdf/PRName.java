a03f7ef8fcff36e966a9d3b8f66bcf680b492dad
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2002-01-09 15:36:54 +0000

    Javadoc warnings

--- src/com/lowagie/text/pdf/PRName.java
+++ src/com/lowagie/text/pdf/PRName.java
@@ -26,7 +26,6 @@ public class PRName extends PRObject implements Comparable{
      * Constructs a <CODE>PdfName</CODE>-object.
      *
      * @param		name		the new Name.
-     * @exception	BadPdfFormatException	gets thrown	when the name is too long or an illegal character is used.
      */
     
     PRName(String name) {
@@ -71,7 +70,6 @@ public class PRName extends PRObject implements Comparable{
      * @return		the value 0 if the object is a name equal to the name of this object,
      *				a value less than 0 if the argument's name is greater than the name of this object,
      *				a value greater than 0 if the argument's name is less than the name of this object
-     * @throws		a <CODE>ClassCastException</CODE> if the argument is not a PdfName
      *
      */
     
