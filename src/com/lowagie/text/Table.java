17a6e1167bd367b899311c93ed8799e8bb96a6b6
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2002-02-22 09:23:43 +0000

    corrected License

--- src/com/lowagie/text/Table.java
+++ src/com/lowagie/text/Table.java
@@ -24,14 +24,14 @@
  * where applicable.
  *
  * Alternatively, the contents of this file may be used under the terms of the
- * LGPL license (the ?GNU LIBRARY GENERAL PUBLIC LICENSE?), in which case the
+ * LGPL license (the "GNU LIBRARY GENERAL PUBLIC LICENSE"), in which case the
  * provisions of LGPL are applicable instead of those above.  If you wish to
  * allow use of your version of this file only under the terms of the LGPL
  * License and not to allow others to use your version of this file under
  * the MPL, indicate your decision by deleting the provisions above and
  * replace them with the notice and other provisions required by the LGPL.
  * If you do not delete the provisions above, a recipient may use your version
- * of this file under either the MPL or the GNU LIBRARY GENERAL PUBLIC LICENSE.
+ * of this file under either the MPL or the"GNU LIBRARY GENERAL PUBLIC LICENSE"
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MPL as stated above or under the terms of the GNU
@@ -40,7 +40,7 @@
  *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Library general Public License for more
+ * FOR A PARTICULAR PURPOSE. See the"GNU LIBRARY GENERAL PUBLIC LICENSE"for more
  * details.
  *
  * If you didn't download this code from the following link, you should check if
