2e576a2524df7b93e8a6fd6b520968c31374e9ff
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2002-02-22 09:26:01 +0000

    corrected license (bis)

--- src/com/lowagie/text/Row.java
+++ src/com/lowagie/text/Row.java
@@ -31,7 +31,7 @@
  * the MPL, indicate your decision by deleting the provisions above and
  * replace them with the notice and other provisions required by the LGPL.
  * If you do not delete the provisions above, a recipient may use your version
- * of this file under either the MPL or the"GNU LIBRARY GENERAL PUBLIC LICENSE"
+ * of this file under either the MPL or the GNU LIBRARY GENERAL PUBLIC LICENSE
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MPL as stated above or under the terms of the GNU
@@ -40,7 +40,7 @@
  *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the"GNU LIBRARY GENERAL PUBLIC LICENSE"for more
+ * FOR A PARTICULAR PURPOSE. See the GNU LIBRARY GENERAL PUBLIC LICENSE for more
  * details.
  *
  * If you didn't download this code from the following link, you should check if
