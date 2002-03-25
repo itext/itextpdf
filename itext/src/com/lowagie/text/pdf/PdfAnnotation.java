2b58e9cb308e24bb4718b47e292469ca08382ea1
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2002-03-25 10:56:45 +0000

    correction typo
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@757 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/pdf/PdfAnnotation.java
+++ itext/src/com/lowagie/text/pdf/PdfAnnotation.java
@@ -10,7 +10,7 @@
  *
  * Software distributed under the License is distributed on an "AS IS" basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the speciPdfAppearanceic language governing rights and limitations under the License.
+ * for the specific language governing rights and limitations under the License.
  *
  * The Original Code is 'iText, a free JAVA-PDF library'.
  *
