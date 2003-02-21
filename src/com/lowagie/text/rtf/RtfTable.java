76c607dd2d4111c9b5ccf5b1003d28010d5b9857
Mark Hall <mark.hall@mail.room3b.eu>
2003-02-21 14:07:23 +0000

    Added package access to the internal Table object

--- src/com/lowagie/text/rtf/RtfTable.java
+++ src/com/lowagie/text/rtf/RtfTable.java
@@ -24,7 +24,7 @@
  * where applicable.
  *
  * Alternatively, the contents of this file may be used under the terms of the
- * LGPL license (the “GNU LIBRARY GENERAL PUBLIC LICENSE”), in which case the
+ * LGPL license (the ?GNU LIBRARY GENERAL PUBLIC LICENSE?), in which case the
  * provisions of LGPL are applicable instead of those above.  If you wish to
  * allow use of your version of this file only under the terms of the LGPL
  * License and not to allow others to use your version of this file under
@@ -64,111 +64,111 @@ import java.io.*;
  * Parts of this Class were contributed by Steffen Stundzig. Many thanks for the
  * improvements.
  */
-public class RtfTable
-{
-  /** Stores the different rows. */
-  private ArrayList rowsList = new ArrayList();
-  /** Stores the RtfWriter, which created this RtfTable. */
-  private RtfWriter writer = null;
-  // <!-- steffen
-  /** Stores the Table, which this RtfTable is based on. */
-  private Table origTable = null;
-  // -->
-  
-  
-
-  /** 
-   * Create a new <code>RtfTable</code>.
-   *
-   * @param writer The <code>RtfWriter</code> that created this Table
-   */
-  public RtfTable(RtfWriter writer)
-  {
-    super();
-    this.writer = writer;
-  }
-
-  /** 
-   * Import a <CODE>Table</CODE> into the <CODE>RtfTable</CODE>.
-   * <P>
-   * @param table A <code>Table</code> specifying the <code>Table</code> to be imported
-   * @param pageWidth An <code>int</code> specifying the page width
-   */
-  public boolean importTable(Table table, int pageWidth)
-  {
+public class RtfTable {
+    /** Stores the different rows. */
+    private ArrayList rowsList = new ArrayList();
+    /** Stores the RtfWriter, which created this RtfTable. */
+    private RtfWriter writer = null;
     // <!-- steffen
-    origTable = table;
-    // -->        
-    // All Cells are pregenerated first, so that cell and rowspanning work
-    Iterator rows = table.iterator();
-    Row row = null;
-
-    int tableWidth = (int) table.widthPercentage();
-    int cellpadding = (int) (table.cellpadding() * writer.twipsFactor);
-    int cellspacing = (int) (table.cellspacing() * writer.twipsFactor);
-    float[] propWidths = table.getProportionalWidths();
-
-    int borders = table.border();
-    java.awt.Color borderColor = table.borderColor();
-    float borderWidth = table.borderWidth();
-
-    for(int i = 0; i < table.size(); i++)
-      {
-        RtfRow rtfRow = new RtfRow(writer, this);
-        rtfRow.pregenerateRows(table.columns());
-        rowsList.add(rtfRow);
-      }
-    int i = 0;
-    while(rows.hasNext())
-      {
-        row = (Row) rows.next();
-        RtfRow rtfRow = (RtfRow) rowsList.get(i);
-        // steffen
-        // <---
-        rtfRow.importRow(row, propWidths, tableWidth, pageWidth, cellpadding, cellspacing, borders, borderColor, borderWidth, i);
-        // -->
-        i++;
-      }
-    return true;
-  }
-
-  /**
-   * Output the content of the <CODE>RtfTable</CODE> to an OutputStream.
-   *
-   * @param os The <code>OutputStream</code> that the content of the <code>RtfTable</code> is to be written to
-   */
-  public boolean writeTable(ByteArrayOutputStream os) throws DocumentException, IOException
-  {
-    // <!-- steffen
-    int size = rowsList.size();
-    for (int i = 0; i < size; i++)
-      {
-        RtfRow row = (RtfRow)rowsList.get( i );
-        row.writeRow( os, i, origTable );
-        os.write((byte) '\n');
-      }
+    /** Stores the Table, which this RtfTable is based on. */
+    private Table origTable = null;
     // -->
-    if(!writer.writingHeaderFooter())
-    {
-      os.write(RtfWriter.escape);
-      os.write(RtfWriter.paragraphDefaults);
-      os.write(RtfWriter.escape);
-      os.write(RtfWriter.paragraph);
-    }
-    return true;
-  }
 
-  /**
-   * <code>RtfCell</code>s call this method to specify that a certain other cell is to be merged with it.
-   *
-   * @param x The column position of the cell to be merged
-   * @param y The row position of the cell to be merged
-   * @param mergeType The merge type specifies the kind of merge to be applied (MERGE_HORIZ_PREV, MERGE_VERT_PREV, MERGE_BOTH_PREV)
-   * @param mergeCell The <code>RtfCell</code> that the cell at x and y is to be merged with
-   */
-  public void setMerge(int x, int y, int mergeType, RtfCell mergeCell)
-  {
-    RtfRow row = (RtfRow) rowsList.get(y);
-    row.setMerge(x, mergeType, mergeCell);
-  }
+
+
+    /**
+     * Create a new <code>RtfTable</code>.
+     *
+     * @param writer The <code>RtfWriter</code> that created this Table
+     */
+    public RtfTable(RtfWriter writer) {
+        super();
+        this.writer = writer;
+    }
+
+    /**
+     * Import a <CODE>Table</CODE> into the <CODE>RtfTable</CODE>.
+     * <P>
+     * @param table A <code>Table</code> specifying the <code>Table</code> to be imported
+     * @param pageWidth An <code>int</code> specifying the page width
+     */
+    public boolean importTable(Table table, int pageWidth) {
+        // <!-- steffen
+        origTable = table;
+        // -->
+        // All Cells are pregenerated first, so that cell and rowspanning work
+        Iterator rows = table.iterator();
+        Row row = null;
+
+        int tableWidth = (int) table.widthPercentage();
+        int cellpadding = (int) (table.cellpadding() * writer.twipsFactor);
+        int cellspacing = (int) (table.cellspacing() * writer.twipsFactor);
+        float[] propWidths = table.getProportionalWidths();
+
+        int borders = table.border();
+        java.awt.Color borderColor = table.borderColor();
+        float borderWidth = table.borderWidth();
+
+        for (int i = 0; i < table.size(); i++) {
+            RtfRow rtfRow = new RtfRow(writer, this);
+            rtfRow.pregenerateRows(table.columns());
+            rowsList.add(rtfRow);
+        }
+        int i = 0;
+        while (rows.hasNext()) {
+            row = (Row) rows.next();
+            RtfRow rtfRow = (RtfRow) rowsList.get(i);
+            // steffen
+            // <---
+            rtfRow.importRow(row, propWidths, tableWidth, pageWidth, cellpadding, cellspacing, borders, borderColor, borderWidth, i);
+            // -->
+            i++;
+        }
+        return true;
+    }
+
+    /**
+     * Output the content of the <CODE>RtfTable</CODE> to an OutputStream.
+     *
+     * @param os The <code>OutputStream</code> that the content of the <code>RtfTable</code> is to be written to
+     */
+    public boolean writeTable(ByteArrayOutputStream os) throws DocumentException, IOException {
+        // <!-- steffen
+        int size = rowsList.size();
+        for (int i = 0; i < size; i++) {
+            RtfRow row = (RtfRow) rowsList.get(i);
+            row.writeRow(os, i, origTable);
+            os.write((byte) '\n');
+        }
+        // -->
+        if (!writer.writingHeaderFooter()) {
+            os.write(RtfWriter.escape);
+            os.write(RtfWriter.paragraphDefaults);
+            os.write(RtfWriter.escape);
+            os.write(RtfWriter.paragraph);
+        }
+        return true;
+    }
+
+    /**
+     * <code>RtfCell</code>s call this method to specify that a certain other cell is to be merged with it.
+     *
+     * @param x The column position of the cell to be merged
+     * @param y The row position of the cell to be merged
+     * @param mergeType The merge type specifies the kind of merge to be applied (MERGE_HORIZ_PREV, MERGE_VERT_PREV, MERGE_BOTH_PREV)
+     * @param mergeCell The <code>RtfCell</code> that the cell at x and y is to be merged with
+     */
+    public void setMerge(int x, int y, int mergeType, RtfCell mergeCell) {
+        RtfRow row = (RtfRow) rowsList.get(y);
+        row.setMerge(x, mergeType, mergeCell);
+    }
+
+    /**
+     * This method allows access to the original Table that led to this RtfTable.
+     *
+     * @return The Table object that is the basis of this RtfTable.
+     */
+    protected Table getOriginalTable() {
+        return origTable;
+    }
 }
