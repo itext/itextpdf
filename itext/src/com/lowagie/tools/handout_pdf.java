4859bf377e2a4b5c6252b4330967522e8fb7f4f9
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2003-06-25 07:35:29 +0000

    removed itext@lowagie.com
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@1119 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/tools/handout_pdf.java
+++ itext/src/com/lowagie/tools/handout_pdf.java
@@ -10,8 +10,6 @@
  * This code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * itext@lowagie.com
  */
 
 /**
