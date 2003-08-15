1e8d0010834daf90fedf8b8a08a04c53339e5ea0
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2003-08-15 15:29:09 +0000

    eclipse suggestions

--- src/com/lowagie/text/pdf/BarcodeEANSUPP.java
+++ src/com/lowagie/text/pdf/BarcodeEANSUPP.java
@@ -45,11 +45,9 @@
  * http://www.lowagie.com/iText/
  */
 package com.lowagie.text.pdf;
-import com.lowagie.text.Rectangle;
 import java.awt.Color;
-import java.awt.Image;
-import java.awt.Canvas;
-import java.awt.image.MemoryImageSource;
+
+import com.lowagie.text.Rectangle;
 
 /** This class takes 2 barcodes, an EAN/UPC and a supplemental
  * and creates a single barcode with both combined in the
