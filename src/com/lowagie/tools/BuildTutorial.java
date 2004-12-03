002d418c28c76a5ba506a72b6d450da5ed38bf8e
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2004-12-03 15:42:04 +0000

    just testing

--- src/com/lowagie/tools/BuildTutorial.java
+++ src/com/lowagie/tools/BuildTutorial.java
@@ -18,73 +18,94 @@ import java.io.*;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
 
-
 /**
  * This class can be used to build the iText website.
- *
+ * 
  * @author Bruno Lowagie (based on an example found in the Developer's Almanac)
  */
 public class BuildTutorial {
 
+	public static class XmlFileFilter implements FilenameFilter {
+		public boolean accept(File dir, String name) {
+			if (name.endsWith(".xml"))
+				return true;
+			return false;
+		}
+	}
 
-  public static class XmlFileFilter implements FilenameFilter {
-    public boolean accept(File dir, String name) {
-      if (name.endsWith(".xml")) return true;
-      return false;
-    }
-  }
-  
-	//~ Methods ----------------------------------------------------------------
-
-  /**
-   * Main method so you can call the convert method from the command line.
-   */
-   
-  public static void main(String[] args) {
-    if (args.length == 3) {
-      File xmlDirectory = new File(args[0]);
-      File htmlDirectory = new File(args[2]);
-      File xslFile = new File(xmlDirectory, args[1]);
-      File xmlFile;
-      String htmlFile;
-      File[] xmlFiles = xmlDirectory.listFiles(new XmlFileFilter());
-      for (int i = 0; i < xmlFiles.length; i++) {
-        xmlFile = xmlFiles[i];
-        htmlFile = xmlFile.getName().substring(0, xmlFile.getName().length() - 4) + ".html";
-        convert(xmlFile, xslFile, new File(htmlDirectory, htmlFile));
-      }
-    }
-    else {
-      System.err.println("Wrong number of parameters.\nUsage: BuildSite xmlDirectory xslFileName htmlDirectory");
-    }
-  }
+	//~ Methods
+	// ----------------------------------------------------------------
 
 	/**
-	 * Converts an <code>infile</code>, using an <code>xslfile</code>
-   * to an <code>outfile</code>.
-	 *
-	 * @param inFilename the path to an XML file
-	 * @param outFilename the path for the output file
-	 * @param xslFilename the path to the XSL file
+	 * Main method so you can call the convert method from the command line.
 	 */
-	public static void convert (File infile, File xslfile, File outfile) {
+
+	public static void main(String[] args) {
+		if (args.length == 4) {
+			File srcdir = new File(args[0]);
+			File destdir = new File(args[1]);
+			File xsl_examples = new File(srcdir, args[2]);
+			File xsl_site = new File(srcdir, args[3]);
+			action(srcdir, destdir, xsl_examples, xsl_site);
+		} else {
+			System.err
+					.println("Wrong number of parameters.\nUsage: BuildSite srcdr destdir xsl_examples xsl_site");
+		}
+	}
+
+	/**
+	 *  Inspects a file or directory that is given and performs the necessary actions on it (transformation or recursion).
+	 */
+	public static void action(File current, File destination, File xsl_examples, File xsl_site) {
+		if (current.isDirectory()) {
+			File[] xmlFiles = current.listFiles();
+			File cursor;
+			File dest = new File(destination, current.getName());
+			dest.mkdir();
+			for (int i = 0; i < xmlFiles.length; i++) {
+				cursor = xmlFiles[i];
+				action(cursor, dest, xsl_examples, xsl_site);
+			}
+		}
+		else if (current.getName().endsWith(".xml")) {
+			if (current.getName().equals("index.xml")) {
+				convert(current, xsl_examples, new File(destination, "build.xml"));
+			}
+			String htmlFile = current.getName().substring(0, current.getName().length() - 4) + ".html";
+			convert(current, xsl_site, new File(destination, htmlFile));
+		}
+	}
+	
+	/**
+	 * Converts an <code>infile</code>, using an <code>xslfile</code> to an
+	 * <code>outfile</code>.
+	 * 
+	 * @param inFilename
+	 *            the path to an XML file
+	 * @param outFilename
+	 *            the path for the output file
+	 * @param xslFilename
+	 *            the path to the XSL file
+	 */
+	public static void convert(File infile, File xslfile, File outfile) {
 		try {
 			// Create transformer factory
-			TransformerFactory factory = TransformerFactory.newInstance ();
+			TransformerFactory factory = TransformerFactory.newInstance();
 
 			// Use the factory to create a template containing the xsl file
-			Templates template = factory.newTemplates (new StreamSource(
-						new FileInputStream(xslfile)));
+			Templates template = factory.newTemplates(new StreamSource(
+					new FileInputStream(xslfile)));
 
 			// Use the template to create a transformer
-			Transformer xformer = template.newTransformer ();
+			Transformer xformer = template.newTransformer();
 
 			// Prepare the input and output files
 			Source source = new StreamSource(new FileInputStream(infile));
 			Result result = new StreamResult(new FileOutputStream(outfile));
 
-			// Apply the xsl file to the source file and write the result to the output file
-			xformer.transform (source, result);
+			// Apply the xsl file to the source file and write the result to the
+			// output file
+			xformer.transform(source, result);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
