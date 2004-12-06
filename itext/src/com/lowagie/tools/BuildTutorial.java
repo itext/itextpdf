eca0f97fa38ff6721d6d9ea8b3b19ab775584629
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2004-12-06 13:33:07 +0000

    changes to BuildTutorial
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@1319 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/tools/BuildTutorial.java
+++ itext/src/com/lowagie/tools/BuildTutorial.java
@@ -41,12 +41,18 @@ public class BuildTutorial {
 	 */
 
 	public static void main(String[] args) {
+		System.out.println("Building tutorial:");
 		if (args.length == 4) {
 			File srcdir = new File(args[0]);
 			File destdir = new File(args[1]);
 			File xsl_examples = new File(srcdir, args[2]);
 			File xsl_site = new File(srcdir, args[3]);
-			action(srcdir, destdir, xsl_examples, xsl_site);
+			try {
+				action(srcdir, destdir, xsl_examples, xsl_site);
+			}
+			catch(IOException ioe) {
+				ioe.printStackTrace();
+			}
 		} else {
 			System.err
 					.println("Wrong number of parameters.\nUsage: BuildSite srcdr destdir xsl_examples xsl_site");
@@ -56,8 +62,11 @@ public class BuildTutorial {
 	/**
 	 *  Inspects a file or directory that is given and performs the necessary actions on it (transformation or recursion).
 	 */
-	public static void action(File current, File destination, File xsl_examples, File xsl_site) {
+	public static void action(File current, File destination, File xsl_examples, File xsl_site) throws IOException {
+		if ("CVS".equals(current.getName())) return;
+		System.out.println(current.getName());
 		if (current.isDirectory()) {
+			System.out.println(current.getCanonicalPath());
 			File dest = new File(destination, current.getName());
 			dest.mkdir();
 			File cursor;
@@ -68,8 +77,12 @@ public class BuildTutorial {
 			}
 		}
 		else if (current.getName().equals("index.xml")) {
+			System.out.println("... transformed");
 			convert(current, xsl_examples, new File(destination, "build.xml"));
 			convert(current, xsl_site, new File(destination, "index.html"));
+		}
+		else {
+			System.out.println("... skipped");
 		}
 	}
 	
