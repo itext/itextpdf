f963ff56754a0da857aaee609e39c5611310fb23
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2004-12-08 14:03:55 +0000

    changes to BuildTutorial
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@1328 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/tools/BuildTutorial.java
+++ itext/src/com/lowagie/tools/BuildTutorial.java
@@ -25,30 +25,38 @@ import javax.xml.transform.stream.*;
  */
 public class BuildTutorial {
 
-	public static class XmlFileFilter implements FilenameFilter {
-		public boolean accept(File dir, String name) {
-			if (name.endsWith(".xml"))
-				return true;
-			return false;
-		}
-	}
-
+	static String root;
+	static FileWriter build;
+	
 	//~ Methods
 	// ----------------------------------------------------------------
 
 	/**
 	 * Main method so you can call the convert method from the command line.
+	 * @param args 4 arguments are expected:
+	 * <ul><li>a sourcedirectory (root of the tutorial xml-files),
+	 * <li>a destination directory (where the html and build.xml files will be generated),
+	 * <li>an xsl to transform the index.xml into a build.xml
+	 * <li>an xsl to transform the index.xml into am index.html</ul>
 	 */
 
 	public static void main(String[] args) {
-		System.out.println("Building tutorial:");
 		if (args.length == 4) {
 			File srcdir = new File(args[0]);
 			File destdir = new File(args[1]);
 			File xsl_examples = new File(srcdir, args[2]);
 			File xsl_site = new File(srcdir, args[3]);
 			try {
+				System.out.print("Building tutorial: ");
+				root = new File(args[1], srcdir.getName()).getCanonicalPath();
+				System.out.println(root);
+				build = new FileWriter(new File(root, "build.xml"));
+				build.write("<project name=\"tutorial\" default=\"all\" basedir=\".\">\n");
+				build.write("<target name=\"all\">\n");
 				action(srcdir, destdir, xsl_examples, xsl_site);
+				build.write("</target>\n</project>");
+				build.flush();
+				build.close();
 			}
 			catch(IOException ioe) {
 				ioe.printStackTrace();
@@ -60,26 +68,39 @@ public class BuildTutorial {
 	}
 
 	/**
-	 *  Inspects a file or directory that is given and performs the necessary actions on it (transformation or recursion).
+	 * Inspects a file or directory that is given and performs the necessary actions on it (transformation or recursion).
+	 * @param source a sourcedirectory (possibly with a tutorial xml-file)
+	 * @param destination a destination directory (where the html and build.xml file will be generated, if necessary)
+	 * @param xsl_examples an xsl to transform the index.xml into a build.xml
+	 * @param xsl_site an xsl to transform the index.xml into am index.html
+	 * @throws IOException when something goes wrong while reading or creating a file or directory
 	 */
-	public static void action(File current, File destination, File xsl_examples, File xsl_site) throws IOException {
-		if ("CVS".equals(current.getName())) return;
-		System.out.println(current.getName());
-		if (current.isDirectory()) {
-			System.out.println(current.getCanonicalPath());
-			File dest = new File(destination, current.getName());
+	public static void action(File source, File destination, File xsl_examples, File xsl_site) throws IOException {
+		if ("CVS".equals(source.getName())) return;
+		System.out.print(source.getName());
+		if (source.isDirectory()) {
+			System.out.print(" ");
+			System.out.println(source.getCanonicalPath());
+			File dest = new File(destination, source.getName());
 			dest.mkdir();
-			File cursor;
-			File[] xmlFiles = current.listFiles();
+			File current;
+			File[] xmlFiles = source.listFiles();
 			for (int i = 0; i < xmlFiles.length; i++) {
-				cursor = xmlFiles[i];
-				action(cursor, dest, xsl_examples, xsl_site);
+				current = xmlFiles[i];
+				action(current, dest, xsl_examples, xsl_site);
 			}
 		}
-		else if (current.getName().equals("index.xml")) {
+		else if (source.getName().equals("index.xml")) {
 			System.out.println("... transformed");
-			convert(current, xsl_examples, new File(destination, "build.xml"));
-			convert(current, xsl_site, new File(destination, "index.html"));
+			convert(source, xsl_site, new File(destination, "index.html"));
+			File buildfile = new File(destination, "build.xml");
+			String path = buildfile.getCanonicalPath().substring(root.length());
+			path = path.replace(File.separatorChar, '/');
+			if ("/build.xml".equals(path)) return;
+			convert(source, xsl_examples, buildfile);
+			build.write("\t<ant antfile=\"${basedir}");
+			build.write(path);
+			build.write("\" target=\"install\" inheritAll=\"false\" />\n");
 		}
 		else {
 			System.out.println("... skipped");
@@ -90,12 +111,12 @@ public class BuildTutorial {
 	 * Converts an <code>infile</code>, using an <code>xslfile</code> to an
 	 * <code>outfile</code>.
 	 * 
-	 * @param inFilename
+	 * @param infile
 	 *            the path to an XML file
-	 * @param outFilename
-	 *            the path for the output file
-	 * @param xslFilename
+	 * @param xslfile
 	 *            the path to the XSL file
+	 * @param outfile
+	 *            the path for the output file
 	 */
 	public static void convert(File infile, File xslfile, File outfile) {
 		try {
@@ -108,6 +129,17 @@ public class BuildTutorial {
 
 			// Use the template to create a transformer
 			Transformer xformer = template.newTransformer();
+			
+			// passing 2 parameters
+			String branch = outfile.getParentFile().getCanonicalPath().substring(root.length());
+			branch = branch.replace(File.separatorChar, '/');
+			StringBuffer path = new StringBuffer();
+			for (int i = 0; i < branch.length(); i++) {
+				if (branch.charAt(i) == '/') path.append("/..");
+			}
+			
+			xformer.setParameter("branch", branch);
+			xformer.setParameter("root", path.toString());
 
 			// Prepare the input and output files
 			Source source = new StreamSource(new FileInputStream(infile));
