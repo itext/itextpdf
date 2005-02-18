19221abbcdfd882774196c17698446372e17fad6
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2005-02-18 13:16:22 +0000

    Oops wrong copyright statement, sorry Steve
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@1571 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/pdf/MultiColumnText.java
+++ itext/src/com/lowagie/text/pdf/MultiColumnText.java
@@ -2,7 +2,7 @@
  * $Id$
  * $Name$
  *
- * Copyright 2004 Paulo Soares
+ * Copyright 2004 Steve Appling
  *
  * The contents of this file are subject to the Mozilla Public License Version 1.1
  * (the "License"); you may not use this file except in compliance with the License.
