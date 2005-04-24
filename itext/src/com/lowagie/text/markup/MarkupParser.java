d3385539189bbb4a4ff20c5cf9668e0a128b7461
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2005-04-24 10:10:40 +0000

    removeCommend method
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@1709 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/markup/MarkupParser.java
+++ itext/src/com/lowagie/text/markup/MarkupParser.java
@@ -58,173 +58,220 @@ import com.lowagie.text.FontFactory;
 
 /**
  * This class contains several static methods that can be used to parse markup.
- *
- * @author  blowagie
+ * 
+ * @author blowagie
  */
 public class MarkupParser {
-    
-/** Creates new MarkupParser */
-    private MarkupParser() {
-    }
-    
-/**
- * This method parses a String with attributes and returns a Properties object.
- *
- * @param   string   a String of this form: 'key1="value1"; key2="value2";... keyN="valueN" '
- * @return  a Properties object
- */
-    
-    public static Properties parseAttributes(String string) {
-        Properties result = new Properties();
-        if (string == null) return result;
-        StringTokenizer keyValuePairs = new StringTokenizer(string, ";");
-        StringTokenizer keyValuePair;
-        String key;
-        String value;
-        while (keyValuePairs.hasMoreTokens()) {
-            keyValuePair = new StringTokenizer(keyValuePairs.nextToken(), ":");
-            if (keyValuePair.hasMoreTokens()) key = keyValuePair.nextToken().trim();
-            else continue;
-            if (keyValuePair.hasMoreTokens()) value = keyValuePair.nextToken().trim();
-            else continue;
-            if (value.startsWith("\"")) value = value.substring(1);
-            if (value.endsWith("\"")) value = value.substring(0, value.length() - 1);
-            result.setProperty(key, value);
-        }
-        return result;
-    }
-/**
- * This method parses the value of 'font' attribute and returns a Properties object.
- *
- * @param   string   a String of this form: 'style1 ... styleN size/leading font1 ... fontN'
- * @return  a Properties object
- */
-    
-    public static Properties parseFont(String string) {
-        Properties result = new Properties();
-        if (string == null) return result;
-        int pos = 0;
-        String value;
-        string = string.trim();
-        while (string.length() > 0) {
-            pos = string.indexOf(" ", pos);
-            if (pos == -1) {
-                value = string;
-                string = "";
-            }
-            else {
-                value = string.substring(0, pos);
-                string = string.substring(pos).trim();
-            }
-            if (value.equalsIgnoreCase("bold")) {
-                result.setProperty(MarkupTags.CSS_FONTWEIGHT, MarkupTags.CSS_BOLD);
-                continue;
-            }
-            if (value.equalsIgnoreCase("italic")) {
-                result.setProperty(MarkupTags.CSS_FONTSTYLE, MarkupTags.CSS_ITALIC);
-                continue;
-            }
-            if (value.equalsIgnoreCase("oblique")) {
-                result.setProperty(MarkupTags.CSS_FONTSTYLE, MarkupTags.CSS_OBLIQUE);
-                continue;
-            }
-            float f;
-            if ((f = parseLength(value)) > 0) {
-                result.setProperty(MarkupTags.CSS_FONTSIZE, String.valueOf(f) + "pt");
-                int p = value.indexOf("/");
-                if (p > -1 && p < value.length() - 1) {
-                    result.setProperty(MarkupTags.CSS_LINEHEIGHT, String.valueOf(value.substring(p + 1)) + "pt");
-                }
-            }
-            if (value.endsWith(",")) {
-                value = value.substring(0, value.length() - 1);
-                if (FontFactory.contains(value)) {
-                    result.setProperty(MarkupTags.CSS_FONTFAMILY, value);
-                    return result;
-                }
-            }
-            if ("".equals(string) && FontFactory.contains(value)) {
-                result.setProperty(MarkupTags.CSS_FONTFAMILY, value);
-            }
-        }
-        return result;
-    }
-    
-/**
- * Parses a length.
- *
- * @param   string  a length in the form of an optional + or -, followed by a number and a unit.
- * @return  a float
- */
-    
-    public static float parseLength(String string) {
-        int pos = 0;
-        int length = string.length();
-        boolean ok = true;
-        while (ok && pos < length) {
-            switch(string.charAt(pos)) {
-                case '+':
-                case '-':
-                case '0':
-                case '1':
-                case '2':
-                case '3':
-                case '4':
-                case '5':
-                case '6':
-                case '7':
-                case '8':
-                case '9':
-                case '.':
-                    pos++;
-                    break;
-                    default:
-                        ok = false;
-            }
-        }
-        if (pos == 0) return 0f;
-        if (pos == length) return Float.valueOf(string + "f").floatValue();
-        float f = Float.valueOf(string.substring(0, pos) + "f").floatValue();
-        string = string.substring(pos);
-        // inches
-        if (string.startsWith("in")) {
-            return f * 72f;
-        }
-        // centimeters
-        if (string.startsWith("cm")) {
-            return (f / 2.54f) * 72f;
-        }
-        // millimeters
-        if (string.startsWith("mm")) {
-            return (f / 25.4f) * 72f;
-        }
-        // picas
-        if (string.startsWith("pc")) {
-            return f * 12f;
-        }
-        // default: we assume the length was measured in points
-        return f;
-    }
-    
-/**
- * Converts a <CODE>Color</CODE> into a HTML representation of this <CODE>Color</CODE>.
- *
- * @param	color	the <CODE>Color</CODE> that has to be converted.
- * @return	the HTML representation of this <COLOR>Color</COLOR>
- */
-    
-    public static Color decodeColor(String color) {
-        int red = 0;
-        int green = 0;
-        int blue = 0;
-        try {
-            red = Integer.parseInt(color.substring(1, 3), 16);
-            green = Integer.parseInt(color.substring(3, 5), 16);
-            blue = Integer.parseInt(color.substring(5), 16);
-        }
-        catch(Exception sioobe) {
-            // empty on purpose
-        }
-        return new Color(red, green, blue);
-    }
+
+	/** Creates new MarkupParser */
+	private MarkupParser() {
+	}
+
+	/**
+	 * Removes the comments sections of  a String.
+	 * @param string the original String
+	 * @param startComment the String that marks the start of a Comment section
+	 * @param endComment the String that marks the end of a Comment section.
+	 * @return the String stripped of its comment section
+	 */
+	public static String removeComment(String string, String startComment,
+			String endComment) {
+		StringBuffer result = new StringBuffer();
+		int pos = 0;
+		int end = endComment.length();
+		int start = string.indexOf(startComment, pos);
+		while (start > -1) {
+			result.append(string.substring(pos, start));
+			pos = string.indexOf(endComment, start) + end;
+			start = string.indexOf(startComment, pos);
+		}
+		result.append(string.substring(pos));
+		return result.toString();
+	}
+
+	/**
+	 * This method parses a String with attributes and returns a Properties
+	 * object.
+	 * 
+	 * @param string
+	 *            a String of this form: 'key1="value1"; key2="value2";...
+	 *            keyN="valueN" '
+	 * @return a Properties object
+	 */
+
+	public static Properties parseAttributes(String string) {
+		Properties result = new Properties();
+		if (string == null)
+			return result;
+		StringTokenizer keyValuePairs = new StringTokenizer(string, ";");
+		StringTokenizer keyValuePair;
+		String key;
+		String value;
+		while (keyValuePairs.hasMoreTokens()) {
+			keyValuePair = new StringTokenizer(keyValuePairs.nextToken(), ":");
+			if (keyValuePair.hasMoreTokens())
+				key = keyValuePair.nextToken().trim();
+			else
+				continue;
+			if (keyValuePair.hasMoreTokens())
+				value = keyValuePair.nextToken().trim();
+			else
+				continue;
+			if (value.startsWith("\""))
+				value = value.substring(1);
+			if (value.endsWith("\""))
+				value = value.substring(0, value.length() - 1);
+			result.setProperty(key, value);
+		}
+		return result;
+	}
+
+	/**
+	 * This method parses the value of 'font' attribute and returns a Properties
+	 * object.
+	 * 
+	 * @param string
+	 *            a String of this form: 'style1 ... styleN size/leading font1
+	 *            ... fontN'
+	 * @return a Properties object
+	 */
+
+	public static Properties parseFont(String string) {
+		Properties result = new Properties();
+		if (string == null)
+			return result;
+		int pos = 0;
+		String value;
+		string = string.trim();
+		while (string.length() > 0) {
+			pos = string.indexOf(" ", pos);
+			if (pos == -1) {
+				value = string;
+				string = "";
+			} else {
+				value = string.substring(0, pos);
+				string = string.substring(pos).trim();
+			}
+			if (value.equalsIgnoreCase("bold")) {
+				result.setProperty(MarkupTags.CSS_FONTWEIGHT,
+						MarkupTags.CSS_BOLD);
+				continue;
+			}
+			if (value.equalsIgnoreCase("italic")) {
+				result.setProperty(MarkupTags.CSS_FONTSTYLE,
+						MarkupTags.CSS_ITALIC);
+				continue;
+			}
+			if (value.equalsIgnoreCase("oblique")) {
+				result.setProperty(MarkupTags.CSS_FONTSTYLE,
+						MarkupTags.CSS_OBLIQUE);
+				continue;
+			}
+			float f;
+			if ((f = parseLength(value)) > 0) {
+				result.setProperty(MarkupTags.CSS_FONTSIZE, String.valueOf(f)
+						+ "pt");
+				int p = value.indexOf("/");
+				if (p > -1 && p < value.length() - 1) {
+					result.setProperty(MarkupTags.CSS_LINEHEIGHT, String
+							.valueOf(value.substring(p + 1))
+							+ "pt");
+				}
+			}
+			if (value.endsWith(",")) {
+				value = value.substring(0, value.length() - 1);
+				if (FontFactory.contains(value)) {
+					result.setProperty(MarkupTags.CSS_FONTFAMILY, value);
+					return result;
+				}
+			}
+			if ("".equals(string) && FontFactory.contains(value)) {
+				result.setProperty(MarkupTags.CSS_FONTFAMILY, value);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Parses a length.
+	 * 
+	 * @param string
+	 *            a length in the form of an optional + or -, followed by a
+	 *            number and a unit.
+	 * @return a float
+	 */
+
+	public static float parseLength(String string) {
+		int pos = 0;
+		int length = string.length();
+		boolean ok = true;
+		while (ok && pos < length) {
+			switch (string.charAt(pos)) {
+			case '+':
+			case '-':
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+			case '.':
+				pos++;
+				break;
+			default:
+				ok = false;
+			}
+		}
+		if (pos == 0)
+			return 0f;
+		if (pos == length)
+			return Float.valueOf(string + "f").floatValue();
+		float f = Float.valueOf(string.substring(0, pos) + "f").floatValue();
+		string = string.substring(pos);
+		// inches
+		if (string.startsWith("in")) {
+			return f * 72f;
+		}
+		// centimeters
+		if (string.startsWith("cm")) {
+			return (f / 2.54f) * 72f;
+		}
+		// millimeters
+		if (string.startsWith("mm")) {
+			return (f / 25.4f) * 72f;
+		}
+		// picas
+		if (string.startsWith("pc")) {
+			return f * 12f;
+		}
+		// default: we assume the length was measured in points
+		return f;
+	}
+
+	/**
+	 * Converts a <CODE>Color</CODE> into a HTML representation of this <CODE>
+	 * Color</CODE>.
+	 * 
+	 * @param color
+	 *            the <CODE>Color</CODE> that has to be converted.
+	 * @return the HTML representation of this <COLOR>Color </COLOR>
+	 */
+
+	public static Color decodeColor(String color) {
+		int red = 0;
+		int green = 0;
+		int blue = 0;
+		try {
+			red = Integer.parseInt(color.substring(1, 3), 16);
+			green = Integer.parseInt(color.substring(3, 5), 16);
+			blue = Integer.parseInt(color.substring(5), 16);
+		} catch (Exception sioobe) {
+			// empty on purpose
+		}
+		return new Color(red, green, blue);
+	}
 }
