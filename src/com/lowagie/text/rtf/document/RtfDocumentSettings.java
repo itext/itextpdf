992d0313157d0d1de71ad12fe8bd3e4b32ad88f0
Mark Hall <mark.hall@mail.room3b.eu>
2005-05-24 16:21:33 +0000

    The RtfDocumentSettings contains document generation settings

---
+++ src/com/lowagie/text/rtf/document/RtfDocumentSettings.java
@@ -0,0 +1,113 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * Copyright 2003, 2004, 2005 by Mark Hall
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is 'iText, a free JAVA-PDF library'.
+ *
+ * The Initial Developer of the Original Code is Bruno Lowagie. Portions created by
+ * the Initial Developer are Copyright (C) 1999, 2000, 2001, 2002 by Bruno Lowagie.
+ * All Rights Reserved.
+ * Co-Developer of the code is Paulo Soares. Portions created by the Co-Developer
+ * are Copyright (C) 2000, 2001, 2002 by Paulo Soares. All Rights Reserved.
+ *
+ * Contributor(s): all the names of the contributors are added in the source code
+ * where applicable.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the
+ * LGPL license (the ?GNU LIBRARY GENERAL PUBLIC LICENSE?), in which case the
+ * provisions of LGPL are applicable instead of those above.  If you wish to
+ * allow use of your version of this file only under the terms of the LGPL
+ * License and not to allow others to use your version of this file under
+ * the MPL, indicate your decision by deleting the provisions above and
+ * replace them with the notice and other provisions required by the LGPL.
+ * If you do not delete the provisions above, a recipient may use your version
+ * of this file under either the MPL or the GNU LIBRARY GENERAL PUBLIC LICENSE.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MPL as stated above or under the terms of the GNU
+ * Library General Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library general Public License for more
+ * details.
+ *
+ * If you didn't download this code from the following link, you should check if
+ * you aren't using an obsolete version:
+ * http://www.lowagie.com/iText/
+ */
+
+package com.lowagie.text.rtf.document;
+
+
+/**
+ * The RtfDocumentSettings contains output specific settings. These settings modify
+ * how the actual document is then generated and some settings may mean that some
+ * RTF readers can't read the document or render it wrongly.
+ * 
+ * @version $Revision$
+ * @author Mark Hall (mhall@edu.uni-klu.ac.at)
+ */
+public class RtfDocumentSettings {
+
+    /**
+     * Whether to also output the table row definition after the cell content.
+     */
+    private boolean outputTableRowDefinitionAfter = true;
+    /**
+     * Whether to output the line breaks that make the rtf document source more readable.
+     */
+    private boolean outputDebugLineBreaks = true;
+
+    
+    /**
+     * Gets whether to output the line breaks for increased rtf document readability.
+     * 
+     * @return Whether to output line breaks.
+     */
+    public boolean isOutputDebugLineBreaks() {
+        return outputDebugLineBreaks;
+    }
+    
+    /**
+     * Sets whether to output the line breaks for increased rtf document readability.
+     * Some line breaks may be added where the rtf specification demands it.
+     * 
+     * @param outputDebugLineBreaks The outputDebugLineBreaks to set.
+     */
+    public void setOutputDebugLineBreaks(boolean outputDebugLineBreaks) {
+        this.outputDebugLineBreaks = outputDebugLineBreaks;
+    }
+    
+    /**
+     * Gets whether the table row definition should also be written after the cell content.
+     * 
+     * @return Returns the outputTableRowDefinitionAfter.
+     */
+    public boolean isOutputTableRowDefinitionAfter() {
+        return outputTableRowDefinitionAfter;
+    }
+    
+    /**
+     * Sets whether the table row definition should also be written after the cell content.
+     * This is recommended to be set to <code>true</code> if you need Word2000 compatiblity and
+     * <code>false</code> if the document should be opened in OpenOffice.org Writer.
+     * 
+     * @param outputTableRowDefinitionAfter The outputTableRowDefinitionAfter to set.
+     */
+    public void setOutputTableRowDefinitionAfter(
+            boolean outputTableRowDefinitionAfter) {
+        this.outputTableRowDefinitionAfter = outputTableRowDefinitionAfter;
+    }
+}
