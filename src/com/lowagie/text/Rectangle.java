ee4c5a02040ba4a26fb3e48467833043f673d3e8
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2005-08-09 14:07:29 +0000

    update toString method of Rectangle

--- src/com/lowagie/text/Rectangle.java
+++ src/com/lowagie/text/Rectangle.java
@@ -946,9 +946,9 @@ public class Rectangle implements Element, MarkupAttributes {
 		buf.append(width());
 		buf.append("x");
 		buf.append(height());
-		buf.append(" rotated ");
+		buf.append(" (rot: ");
 		buf.append(rotation);
-		buf.append(" degrees");
+		buf.append(" degrees)");
 		return buf.toString();
 	}
 
