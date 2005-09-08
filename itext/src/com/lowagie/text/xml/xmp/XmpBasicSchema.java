fbb97f42619bc234c0793d6b345e818f6ee04410
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2005-09-08 07:50:15 +0000

    Some bugfixes and suggestions by Christopher MacKenzie were implemented in the XMP package
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@1932 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/xml/xmp/XmpBasicSchema.java
+++ itext/src/com/lowagie/text/xml/xmp/XmpBasicSchema.java
@@ -56,6 +56,11 @@ import java.io.IOException;
  * An implementation of an XmpSchema.
  */
 public class XmpBasicSchema extends XmpSchema {
+
+	/** default namespace identifier*/
+	public static final String DEFAULT_XPATH_ID = "xmp";
+	/** default namespace uri*/
+	public static final String DEFAULT_XPATH_URI = "http://ns.adobe.com/xap/1.0/";
 	
 	/** An unordered array specifying properties that were edited outside the authoring application. Each item should contain a single namespace and XPath separated by one ASCII space (U+0020). */
 	public static final String ADVISORY = "xmp:Advisory";
@@ -66,29 +71,29 @@ public class XmpBasicSchema extends XmpSchema {
 	/** The name of the first known tool used to create the resource. If history is present in the metadata, this value should be equivalent to that of xmpMM:History’s softwareAgent property. */
 	public static final String CREATORTOOL = "xmp:CreatorTool";
 	/** An unordered array of text strings that unambiguously identify the resource within a given context. */
-	public static final String IDENTIFIER = "xmp:identifier";
+	public static final String IDENTIFIER = "xmp:Identifier";
 	/** The date and time that any metadata for this resource was last changed. */
 	public static final String METADATADATE = "xmp:MetadataDate";
 	/** The date and time the resource was last modified. */
-	public static final String MODIFYDATE = "xml:ModifyDate";
+	public static final String MODIFYDATE = "xmp:ModifyDate";
 	/** A short informal name for the resource. */
 	public static final String NICKNAME = "xmp:Nickname";
 	/** An alternative array of thumbnail images for a file, which can differ in characteristics such as size or image encoding. */
 	public static final String THUMBNAILS = "xmp:Thumbnails";
+
 	
 	/**
-	 * @param shorthand
 	 * @throws IOException
 	 */
 	public XmpBasicSchema() throws IOException {
-		super("xmlns:dc='http://ns.adobe.com/xap/1.0'");
+		super("xmlns:" + DEFAULT_XPATH_ID + "=\"" + DEFAULT_XPATH_URI + "\"");
 	}
 	
 	/**
 	 * Adds the creatortool.
 	 * @param creator
 	 */
-	public void addCreator(String creator) {
+	public void addCreatorTool(String creator) {
 		setProperty(CREATORTOOL, creator);
 	}
 	
@@ -96,7 +101,7 @@ public class XmpBasicSchema extends XmpSchema {
 	 * Adds the creation date.
 	 * @param date
 	 */
-	public void addCreationDate(String date) {
+	public void addCreateDate(String date) {
 		setProperty(CREATEDATE, date);
 	}
 	
@@ -106,5 +111,31 @@ public class XmpBasicSchema extends XmpSchema {
 	 */
 	public void addModDate(String date) {
 		setProperty(MODIFYDATE, date);
+	}
+
+	/**
+	 * Adds the meta data date.
+	 * @param date
+	 */
+	public void addMetaDataDate(String date) {
+		setProperty(METADATADATE, date);
+	}
+
+	/** Adds the identifier.
+	 * @param id
+	 */
+	public void addIdentifiers(String[] id) {
+		XmpArray array = new XmpArray(XmpArray.UNORDERED);
+		for (int i = 0; i < id.length; i++) {
+			array.add(id[i]);
+		}
+		setProperty(IDENTIFIER, array);
+	}
+
+	/** Adds the nickname.
+	 * @param name
+	 */
+	public void addNickname(String name) {
+		setProperty(NICKNAME, name);
 	}
 }
