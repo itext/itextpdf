8bee193ee3165d321a27679e9ac86ae00bd26385
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2005-10-20 09:43:20 +0000

    preparing 1.3.5 release

--- src/com/lowagie/text/Document.java
+++ src/com/lowagie/text/Document.java
@@ -105,7 +105,7 @@ public class Document implements DocListener {
     // membervariables
     
 	/** This constant may only be changed by Paulo Soares and/or Bruno Lowagie. */
-	private static final String ITEXT_VERSION = "iText1.3.4 by lowagie.com";
+	private static final String ITEXT_VERSION = "iText1.3.5 by lowagie.com (based on version paulo-155)";
     
 	/**
 	 * Allows the pdf documents to be produced without compression for debugging
