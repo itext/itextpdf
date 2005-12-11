a327fa74c3ae6ff326d9736f2016ec58bdf145c5
Paulo Soares <pgpsoares@gmail.com>
2005-12-11 19:33:39 +0000

    PdfReader.killIndirect() was made public
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@2085 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/pdf/PdfReader.java
+++ itext/src/com/lowagie/text/pdf/PdfReader.java
@@ -1028,7 +1028,13 @@ public class PdfReader {
         }
     }
     
-    static PdfObject killIndirect(PdfObject obj) {
+    /**
+     * Eliminates the reference to the object freeing the memory used by it and clearing
+     * the xref entry.
+     * @param obj the object. If it's an indirect reference it will be eliminated
+     * @return the object or the already erased dereferenced object
+     */    
+    public static PdfObject killIndirect(PdfObject obj) {
         if (obj == null || obj.isNull())
             return null;
         PdfObject ret = getPdfObjectRelease(obj);
