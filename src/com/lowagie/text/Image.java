9ada561efb9c8d603ff6a9b34bcd5a5d1b9223cc
Paulo Soares <psoares@gmail.com>
2006-01-23 17:54:20 +0000

    missing dispose()

--- src/com/lowagie/text/Image.java
+++ src/com/lowagie/text/Image.java
@@ -713,40 +713,41 @@ public abstract class Image extends Rectangle implements Element,
 		return getInstance(writer.getDirectContent(), awtImage, quality);
 	}
 	
-	/**
-	 * Gets an instance of a Image from a java.awt.Image.
-	 * The image is added as a JPEG with a userdefined quality.
-	 * 
-	 * @param cb
-	 *            the <CODE>PdfContentByte</CODE> object to which the image will be added
-	 * @param image
-	 *            the <CODE>java.awt.Image</CODE> to convert
-	 * @param quality
-	 *            a float value between 0 and 1
-	 * @return an object of type <CODE>PdfTemplate</CODE>
-	 * @throws BadElementException
-	 *             on error
-	 * @throws IOException
-	 */
-	public static Image getInstance(PdfContentByte cb, java.awt.Image awtImage, float quality) throws BadElementException, IOException {
-		java.awt.image.PixelGrabber pg = new java.awt.image.PixelGrabber(awtImage,
-				0, 0, -1, -1, true);
-		try {
-			pg.grabPixels();
-		} catch (InterruptedException e) {
-			throw new IOException(
-					"java.awt.Image Interrupted waiting for pixels!");
-		}
-		if ((pg.getStatus() & java.awt.image.ImageObserver.ABORT) != 0) {
-			throw new IOException("java.awt.Image fetch aborted or errored");
-		}
-		int w = pg.getWidth();
-		int h = pg.getHeight();
-		PdfTemplate tp = cb.createTemplate(w, h);
-		Graphics2D g2d = tp.createGraphics(w, h, true, quality);
-		g2d.drawImage(awtImage, 0, 0, null);
-		return getInstance(tp);
-	}
+    /**
+     * Gets an instance of a Image from a java.awt.Image.
+     * The image is added as a JPEG with a userdefined quality.
+     *
+     * @param cb
+     *            the <CODE>PdfContentByte</CODE> object to which the image will be added
+     * @param image
+     *            the <CODE>java.awt.Image</CODE> to convert
+     * @param quality
+     *            a float value between 0 and 1
+     * @return an object of type <CODE>PdfTemplate</CODE>
+     * @throws BadElementException
+     *             on error
+     * @throws IOException
+     */
+    public static Image getInstance(PdfContentByte cb, java.awt.Image awtImage, float quality) throws BadElementException, IOException {
+        java.awt.image.PixelGrabber pg = new java.awt.image.PixelGrabber(awtImage,
+                0, 0, -1, -1, true);
+        try {
+            pg.grabPixels();
+        } catch (InterruptedException e) {
+            throw new IOException(
+                    "java.awt.Image Interrupted waiting for pixels!");
+        }
+        if ((pg.getStatus() & java.awt.image.ImageObserver.ABORT) != 0) {
+            throw new IOException("java.awt.Image fetch aborted or errored");
+        }
+        int w = pg.getWidth();
+        int h = pg.getHeight();
+        PdfTemplate tp = cb.createTemplate(w, h);
+        Graphics2D g2d = tp.createGraphics(w, h, true, quality);
+        g2d.drawImage(awtImage, 0, 0, null);
+        g2d.dispose();
+        return getInstance(tp);
+    }
 
 	/**
 	 * Gets an instance of an Image.
