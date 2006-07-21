df956670d499d8b27c99f3a35e9ba7188f1c7d4c
Paulo Soares <psoares@gmail.com>
2006-07-21 15:15:54 +0000

    fixed bold and italic output with fonts created from BaseFont

--- src/com/lowagie/text/html/HtmlWriter.java
+++ src/com/lowagie/text/html/HtmlWriter.java
@@ -61,6 +61,7 @@ import java.util.Stack;
 import java.util.EmptyStackException;
 
 import com.lowagie.text.*;
+import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.markup.MarkupTags;
 
 /**
@@ -1055,6 +1056,20 @@ public class HtmlWriter extends DocWriter implements DocListener {
             }
             
             int fontstyle = font.style();
+            BaseFont bf = font.getBaseFont();
+            if (bf != null) {
+                String ps = bf.getPostscriptFontName().toLowerCase();
+                if (ps.indexOf("bold") >= 0) {
+                    if (fontstyle == Font.UNDEFINED)
+                        fontstyle = 0;
+                    fontstyle |= Font.BOLD;
+                }
+                if (ps.indexOf("italic") >= 0 || ps.indexOf("oblique") >= 0) {
+                    if (fontstyle == Font.UNDEFINED)
+                        fontstyle = 0;
+                    fontstyle |= Font.ITALIC;
+                }
+            }
             if (fontstyle != Font.UNDEFINED && fontstyle != Font.NORMAL) {
                 switch (fontstyle & Font.BOLDITALIC) {
                     case Font.BOLD:
