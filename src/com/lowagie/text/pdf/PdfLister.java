c3c438a98da1f14fe28e1a0949338e77f2e48d3d
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2006-08-30 14:15:34 +0000

    license update

--- src/com/lowagie/text/pdf/PdfLister.java
+++ src/com/lowagie/text/pdf/PdfLister.java
@@ -2,7 +2,7 @@
  * $Id$
  * $Name$
  *
- * Copyright 1999, 2000, 2001, 2002 Bruno Lowagie
+ * Copyright 2002 Mark Thompson
  *
  * The contents of this file are subject to the Mozilla Public License Version 1.1
  * (the "License"); you may not use this file except in compliance with the License.
