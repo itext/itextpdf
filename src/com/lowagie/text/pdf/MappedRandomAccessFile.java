e550cd9f526143d22ab0e06c5885d245aa304384
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2006-09-14 10:51:39 +0000

    removed an unused import

--- src/com/lowagie/text/pdf/MappedRandomAccessFile.java
+++ src/com/lowagie/text/pdf/MappedRandomAccessFile.java
@@ -53,7 +53,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.nio.BufferUnderflowException;
-import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.security.AccessController;
