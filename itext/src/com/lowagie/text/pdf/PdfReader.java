88c327c4481640e7faeae25b8e5d4a876fb39093
Paulo Soares <pgpsoares@gmail.com>
2007-03-06 12:30:03 +0000

    added removeUsageRights()
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@2636 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/pdf/PdfReader.java
+++ itext/src/com/lowagie/text/pdf/PdfReader.java
@@ -3293,4 +3293,19 @@ public class PdfReader implements PdfViewerPreferences {
             return null;
         return new PdfIndirectReference(0, cryptoRef.getNumber(), cryptoRef.getGeneration());
     }
+    
+    /**
+     * Removes any usage rights that this PDF may have. Only Adobe can grant usage rights
+     * and any PDF modification with iText will invalidate them. Invalidated usage rights may
+     * confuse Acrobat and it's advisabe to remove them altogether.
+     */
+    public void removeUsageRights() {
+        PdfDictionary perms = (PdfDictionary)getPdfObject(catalog.get(PdfName.PERMS));
+        if (perms == null)
+            return;
+        perms.remove(PdfName.UR);
+        perms.remove(PdfName.UR3);
+        if (perms.size() == 0)
+            catalog.remove(PdfName.PERMS);
+    }
 }
