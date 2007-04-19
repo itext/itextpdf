086347eb4c1231a9e7eaf93276e8c3fe4c93596c
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2007-04-19 11:04:11 +0000

    I think the MPL/LGPL isn't necessary for this class. The original code of the Pfm2Afm functionality was released by IBM for public use as long as the copyright remains intact. I updated the copyright notices to reflect the changes that were made by iText developers.

--- src/com/lowagie/text/pdf/Pfm2afm.java
+++ src/com/lowagie/text/pdf/Pfm2afm.java
@@ -1,49 +1,3 @@
-/*
- * Copyright 2005 by Paulo Soares.
- *
- * The contents of this file are subject to the Mozilla Public License Version 1.1
- * (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the License.
- *
- * The Original Code is 'iText, a free JAVA-PDF library'.
- *
- * The Initial Developer of the Original Code is Bruno Lowagie. Portions created by
- * the Initial Developer are Copyright (C) 1999, 2000, 2001, 2002 by Bruno Lowagie.
- * All Rights Reserved.
- * Co-Developer of the code is Paulo Soares. Portions created by the Co-Developer
- * are Copyright (C) 2000, 2001, 2002 by Paulo Soares. All Rights Reserved.
- *
- * Contributor(s): all the names of the contributors are added in the source code
- * where applicable.
- *
- * Alternatively, the contents of this file may be used under the terms of the
- * LGPL license (the "GNU LIBRARY GENERAL PUBLIC LICENSE"), in which case the
- * provisions of LGPL are applicable instead of those above.  If you wish to
- * allow use of your version of this file only under the terms of the LGPL
- * License and not to allow others to use your version of this file under
- * the MPL, indicate your decision by deleting the provisions above and
- * replace them with the notice and other provisions required by the LGPL.
- * If you do not delete the provisions above, a recipient may use your version
- * of this file under either the MPL or the GNU LIBRARY GENERAL PUBLIC LICENSE.
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the MPL as stated above or under the terms of the GNU
- * Library General Public License as published by the Free Software Foundation;
- * either version 2 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Library general Public License for more
- * details.
- *
- * If you didn't download this code from the following link, you should check if
- * you aren't using an obsolete version:
- * http://www.lowagie.com/iText/
- */
 /********************************************************************
  *                                                                  *
  *  Title:  pfm2afm - Convert Windows .pfm files to .afm files      *
@@ -106,6 +60,34 @@
  *  function from the net (the function was buggy, so I had to fix  *
  *  it, too!).  I also made the program more friendly towards       *
  *  files with mixed-case filenames.                                *
+ *                                                                  *
+ ********************************************************************/
+
+/********************************************************************
+ *                                                                  *
+ *  1/31/2005, by Paulo Soares                                      *
+ *                                                                  *
+ *  This code was integrated into iText.                            * 
+ *  Note that the itoa function mentioned in the comment by Rod     *
+ *  Smith is no longer in the code because Java has native support  *
+ *  in PrintWriter to convert integers to strings                   *
+ *                                                                  *
+ ********************************************************************/
+
+/********************************************************************
+ *                                                                  *
+ *  7/16/2005, by Bruno Lowagie                                     *
+ *                                                                  *
+ *  I solved an Eclipse Warning.                                    *
+ *                                                                  *
+ ********************************************************************/
+
+/********************************************************************
+ *                                                                  *
+ *  9/14/2006, by Xavier Le Vourch                                  *
+ *                                                                  *
+ *  expand import clauses (import java.io.*)                        *                                           
+ *  the removal of an exception in readString was restored on 9/16  *
  *                                                                  *
  ********************************************************************/
 package com.lowagie.text.pdf;
