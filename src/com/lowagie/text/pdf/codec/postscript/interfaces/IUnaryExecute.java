fd4e32ef47f687db5e1fffd80000551123d6d407
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2007-05-20 11:11:16 +0000

    Removal of EPS support (ambiguous license: SUN sample license vs. 'confidential' source code)

--- src/com/lowagie/text/pdf/codec/postscript/interfaces/IUnaryExecute.java
+++
@@ -1,9 +0,0 @@
-package com.lowagie.text.pdf.codec.postscript.interfaces;
-
-import com.lowagie.text.pdf.codec.postscript.PAContext;
-import com.lowagie.text.pdf.codec.postscript.PainterException;
-
-public interface IUnaryExecute {
-  public void execute(PAContext context, Object operand1) throws
-      PainterException;
-}
