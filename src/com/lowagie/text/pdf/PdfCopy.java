a240855608ec86212872f2daafc1a98801d7511a
Paulo Soares <psoares@gmail.com>
2007-07-24 09:36:17 +0000

    javadoc correction

--- src/com/lowagie/text/pdf/PdfCopy.java
+++ src/com/lowagie/text/pdf/PdfCopy.java
@@ -505,12 +505,14 @@ public class PdfCopy extends PdfWriter {
     }
     
     /**
-     * Create a page stamp. This method modifies the PdfReader instance.<p>
+     * Create a page stamp. New content and annotations, including new fields, are allowed.
+     * The fields added cannot have parents in another pages. This method modifies the PdfReader instance.<p>
      * The general usage to stamp something in a page is:
      * <p>
      * <pre>
      * PdfImportedPage page = copy.getImportedPage(reader, 1);
      * PdfCopy.PageStamp ps = copy.createPageStamp(page);
+     * ps.addAnnotation(PdfAnnotation.createText(copy, new Rectangle(50, 180, 70, 200), "Hello", "No Thanks", true, "Comment"));
      * PdfContentByte under = ps.getUnderContent();
      * under.addImage(img);
      * PdfContentByte over = ps.getOverContent();
