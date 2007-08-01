b0b7798f3a49588eb1e8c8351d6cdd1a82d08027
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2007-08-01 08:46:06 +0000

    Possible solution for some margin problems.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@2876 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/com/lowagie/text/pdf/PdfDocument.java
+++ itext/src/com/lowagie/text/pdf/PdfDocument.java
@@ -821,6 +821,7 @@ class PdfDocument extends Document {
         lastElementType = -1;
         isNewpage = true;
         if (writer == null || (writer.getDirectContent().size() == 0 && writer.getDirectContentUnder().size() == 0 && (pageEmpty || writer.isPaused()))) {
+        	setNewPageSizeAndMargins();
             return false;
         }
     	if (!open || close) {
@@ -1090,16 +1091,7 @@ class PdfDocument extends Document {
         textEmptySize = text.size();
 
     	markPoint = 0;
-        if (marginMirroring && (getPageNumber() & 1) == 0) {
-            marginRight = nextMarginLeft;
-            marginLeft = nextMarginRight;
-        }
-        else {
-            marginLeft = nextMarginLeft;
-            marginRight = nextMarginRight;
-        }
-        marginTop = nextMarginTop;
-        marginBottom = nextMarginBottom;
+        setNewPageSizeAndMargins();
         imageEnd = -1;
         indentation.imageIndentRight = 0;
         indentation.imageIndentLeft = 0;
@@ -1108,7 +1100,6 @@ class PdfDocument extends Document {
         currentHeight = 0;
         
         // backgroundcolors, etc...
-        pageSize = nextPageSize;
         thisBoxSize = new HashMap(boxSize);
         if (pageSize.getBackgroundColor() != null
         || pageSize.hasBorders()
@@ -2144,6 +2135,20 @@ class PdfDocument extends Document {
             boxSize.remove(boxName);
         else
             boxSize.put(boxName, new PdfRectangle(size));
+    }
+    
+    private void setNewPageSizeAndMargins() {
+        pageSize = nextPageSize;
+    	if (marginMirroring && (getPageNumber() & 1) == 0) {
+            marginRight = nextMarginLeft;
+            marginLeft = nextMarginRight;
+        }
+        else {
+            marginLeft = nextMarginLeft;
+            marginRight = nextMarginRight;
+        }
+        marginTop = nextMarginTop;
+        marginBottom = nextMarginBottom;
     }
     
     /**
