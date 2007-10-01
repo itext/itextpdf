8545752f980763310e999c07af5d2579b2dabfac
Paulo Soares <psoares@gmail.com>
2007-10-01 22:50:01 +0000

    Checkboxes should behave as Radioboxes specially concerning the Opt array.

--- src/com/lowagie/text/pdf/AcroFields.java
+++ src/com/lowagie/text/pdf/AcroFields.java
@@ -1250,64 +1250,47 @@ public class AcroFields {
                 return true;
             }
             PdfName v = new PdfName(value);
-            if ((flags & PdfFormField.FF_RADIO) == 0) {
-                for (int idx = 0; idx < item.values.size(); ++idx) {
-                    ((PdfDictionary)item.values.get(idx)).put(PdfName.V, v);
-                    markUsed((PdfDictionary)item.values.get(idx));
-                    PdfDictionary merged = (PdfDictionary)item.merged.get(idx);
-                    merged.put(PdfName.V, v);
-                    merged.put(PdfName.AS, v);
-                    PdfDictionary widget = (PdfDictionary)item.widgets.get(idx);
-                    if (isInAP(widget,  v))
-                        widget.put(PdfName.AS, v);
+            ArrayList lopt = new ArrayList();
+            PdfObject opts = PdfReader.getPdfObject(((PdfDictionary)item.values.get(0)).get(PdfName.OPT));
+            if (opts != null && opts.isArray()) {
+                ArrayList list = ((PdfArray)opts).getArrayList();
+                for (int k = 0; k < list.size(); ++k) {
+                    PdfObject vv = PdfReader.getPdfObject((PdfObject)list.get(k));
+                    if (vv != null && vv.isString())
+                        lopt.add(((PdfString)vv).toUnicodeString());
                     else
-                        widget.put(PdfName.AS, PdfName.Off);
-                    markUsed(widget);
+                        lopt.add(null);
                 }
             }
-            else {
-                ArrayList lopt = new ArrayList();
-                PdfObject opts = PdfReader.getPdfObject(((PdfDictionary)item.values.get(0)).get(PdfName.OPT));
-                if (opts != null && opts.isArray()) {
-                    ArrayList list = ((PdfArray)opts).getArrayList();
-                    for (int k = 0; k < list.size(); ++k) {
-                        PdfObject vv = PdfReader.getPdfObject((PdfObject)list.get(k));
-                        if (vv != null && vv.isString())
-                            lopt.add(((PdfString)vv).toUnicodeString());
-                        else
-                            lopt.add(null);
-                    }
+            int vidx = lopt.indexOf(value);
+            PdfName valt = null;
+            PdfName vt;
+            if (vidx >= 0) {
+                vt = valt = new PdfName(String.valueOf(vidx));
+            }
+            else
+                vt = v;
+            for (int idx = 0; idx < item.values.size(); ++idx) {
+                PdfDictionary merged = (PdfDictionary)item.merged.get(idx);
+                PdfDictionary widget = (PdfDictionary)item.widgets.get(idx);
+                markUsed((PdfDictionary)item.values.get(idx));
+                if (valt != null) {
+                    PdfString ps = new PdfString(value, PdfObject.TEXT_UNICODE);
+                    ((PdfDictionary)item.values.get(idx)).put(PdfName.V, ps);
+                    merged.put(PdfName.V, ps);
                 }
-                int vidx = lopt.indexOf(value);
-                PdfName valt = null;
-                PdfName vt;
-                if (vidx >= 0) {
-                    vt = valt = new PdfName(String.valueOf(vidx));
+                else {
+                    ((PdfDictionary)item.values.get(idx)).put(PdfName.V, v);
+                    merged.put(PdfName.V, v);
                 }
-                else
-                    vt = v;
-                for (int idx = 0; idx < item.values.size(); ++idx) {
-                    PdfDictionary merged = (PdfDictionary)item.merged.get(idx);
-                    PdfDictionary widget = (PdfDictionary)item.widgets.get(idx);
-                    markUsed((PdfDictionary)item.values.get(idx));
-                    if (valt != null) {
-                        PdfString ps = new PdfString(value, PdfObject.TEXT_UNICODE);
-                        ((PdfDictionary)item.values.get(idx)).put(PdfName.V, ps);
-                        merged.put(PdfName.V, ps);
-                    }
-                    else {
-                        ((PdfDictionary)item.values.get(idx)).put(PdfName.V, v);
-                        merged.put(PdfName.V, v);
-                    }
-                    markUsed(widget);
-                    if (isInAP(widget,  vt)) {
-                        merged.put(PdfName.AS, vt);
-                        widget.put(PdfName.AS, vt);
-                    }
-                    else {
-                        merged.put(PdfName.AS, PdfName.Off);
-                        widget.put(PdfName.AS, PdfName.Off);
-                    }
+                markUsed(widget);
+                if (isInAP(widget,  vt)) {
+                    merged.put(PdfName.AS, vt);
+                    widget.put(PdfName.AS, vt);
+                }
+                else {
+                    merged.put(PdfName.AS, PdfName.Off);
+                    widget.put(PdfName.AS, PdfName.Off);
                 }
             }
             return true;
