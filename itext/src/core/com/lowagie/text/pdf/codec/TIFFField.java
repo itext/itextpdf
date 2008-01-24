2e2d7bed69c57a1d503d82ce82ff8456b40af790
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2008-01-24 12:20:57 +0000

    After a mail communication with Brian Burkhalter (@ sun.com), the license notices for the classes derived from JAI code was updated. I also discovered that the license notices for TIFFConstants wrongfully attributed the constants to SUN. The license notice for that class was updated too.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@3101 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/core/com/lowagie/text/pdf/codec/TIFFField.java
+++ itext/src/core/com/lowagie/text/pdf/codec/TIFFField.java
@@ -1,34 +1,47 @@
 /*
- * Copyright (c) 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * Copyright 2003-2008 by Paulo Soares.
+ * 
+ * This code was originally released in 2001 by SUN (see class
+ * com.sun.media.imageio.plugins.tiff.TIFFField.java)
+ * using the BSD license in a specific wording. In a mail dating from
+ * January 23, 2008, Brian Burkhalter (@sun.com) gave us permission
+ * to use the code under the following version of the BSD license:
  *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met:
+ * Copyright (c) 2006 Sun Microsystems, Inc. All  Rights Reserved.
  * 
- * -Redistributions of source code must retain the above copyright notice, this 
- * list of conditions and the following disclaimer.
- *
- * -Redistribution in binary form must reproduct the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 
  * 
- * Neither the name of Sun Microsystems, Inc. or the names of contributors may
- * be used to endorse or promote products derived from this software without
- * specific prior written permission.
+ * - Redistribution of source code must retain the above copyright 
+ *   notice, this  list of conditions and the following disclaimer.
  * 
- * This software is provided "AS IS," without a warranty of any kind. ALL
- * EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
- * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR
- * NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN AND ITS LICENSORS SHALL NOT BE
- * LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
- * OR DISTRIBUTING THE SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS
- * LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT,
- * INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
- * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF
- * OR INABILITY TO USE SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGES.
+ * - Redistribution in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in 
+ *   the documentation and/or other materials provided with the
+ *   distribution.
  * 
- * You acknowledge that Software is not designed,licensed or intended for use in 
- * the design, construction, operation or maintenance of any nuclear facility.
+ * Neither the name of Sun Microsystems, Inc. or the names of 
+ * contributors may be used to endorse or promote products derived 
+ * from this software without specific prior written permission.
+ * 
+ * This software is provided "AS IS," without a warranty of any 
+ * kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND 
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY
+ * EXCLUDED. SUN MIDROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL 
+ * NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF 
+ * USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS BE LIABLE FOR 
+ * ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
+ * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
+ * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
+ * INABILITY TO USE THIS SOFTWARE, EVEN IF SUN HAS BEEN ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGES. 
+ * 
+ * You acknowledge that this software is not designed or intended for 
+ * use in the design, construction, operation or maintenance of any 
+ * nuclear facility. 
  */
 package com.lowagie.text.pdf.codec;
 
