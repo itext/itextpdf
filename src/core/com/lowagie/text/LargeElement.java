8cb8bcda63c0725ff737c5944c9e83e6d78993ee
Xavier Le Vourch <xavier@brittanysoftware.com>
2008-02-23 22:51:41 +0000

    javadoc fix: comments used wrong method name

--- src/core/com/lowagie/text/LargeElement.java
+++ src/core/com/lowagie/text/LargeElement.java
@@ -53,18 +53,18 @@ package com.lowagie.text;
 /**
  * Interface implemented by Element objects that can potentially consume
  * a lot of memory. Objects implementing the LargeElement interface can
- * be added to a Document more than once. If you have invoked setCompleted(false),
+ * be added to a Document more than once. If you have invoked setComplete(false),
  * they will be added partially and the content that was added will be
- * removed until you've invoked setCompleted(true);
+ * removed until you've invoked setComplete(true);
  * @since	iText 2.0.8
  */
 
 public interface LargeElement extends Element {
 	
 	/**
-	 * If you invoke setCompleted(false), you indicate that the content
+	 * If you invoke setComplete(false), you indicate that the content
 	 * of the object isn't complete yet; it can be added to the document
-	 * partially, but more will follow. If you invoke setCompleted(true),
+	 * partially, but more will follow. If you invoke setComplete(true),
 	 * you indicate that you won't add any more data to the object.
 	 * @since	iText 2.0.8
 	 * @param	complete	false if you'll be adding more data after
