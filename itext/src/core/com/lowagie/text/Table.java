80b07c3631142b5f220869dec85650fcba990211
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2008-03-09 09:37:41 +0000

    getElement has been made public again (there was a question about it on the mailing list)
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@3164 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/core/com/lowagie/text/Table.java
+++ itext/src/core/com/lowagie/text/Table.java
@@ -957,8 +957,9 @@ public class Table extends Rectangle implements LargeElement {
      * @param row
      * @param column
      * @return  dimension
+     * @since  2.1.0 (was made private in 2.0.3)
      */
-    private Object getElement(int row, int column) {
+    public Object getElement(int row, int column) {
         return ((Row) rows.get(row)).getCell(column);
     }
     
