10b1df838ed75592529478d78a29fc5347eedf81
Mark Hall <mark.hall@mail.room3b.eu>
2008-03-29 19:44:09 +0000

    Fixed a bug in how the list indentation was being calculated.

--- src/rtf/com/lowagie/text/rtf/list/RtfList.java
+++ src/rtf/com/lowagie/text/rtf/list/RtfList.java
@@ -302,10 +302,6 @@ public class RtfList extends RtfElement implements RtfExtendedElement {
             }
         }
         
-        if(this.listLevel == 0) {
-            correctIndentation();
-        }
-        
         fontNumber = new RtfFont(document, new Font(Font.TIMES_ROMAN, 10, Font.NORMAL, new Color(0, 0, 0)));
         if (list.getSymbol() != null && list.getSymbol().getFont() != null && !list.getSymbol().getContent().startsWith("-") && list.getSymbol().getContent().length() > 0) {
             // only set this to bullet symbol is not default
@@ -464,6 +460,10 @@ public class RtfList extends RtfElement implements RtfExtendedElement {
      */    
     public void writeContent(final OutputStream result) throws IOException
     {
+        if(this.listLevel == 0) {
+            correctIndentation();
+        }
+        
         result.write(OPEN_GROUP);
         int itemNr = 0;
         for(int i = 0; i < items.size(); i++) {
@@ -605,6 +605,7 @@ public class RtfList extends RtfElement implements RtfExtendedElement {
      * from the parent RtfList. Also calls correctIndentation on all child RtfLists.
      */
     protected void correctIndentation() {
+        System.out.println(this + ": " + this.firstIndent + ", " + this.leftIndent);
         if(this.parentList != null) {
             this.leftIndent = this.leftIndent + this.parentList.getLeftIndent() + this.parentList.getFirstIndent();
         }
