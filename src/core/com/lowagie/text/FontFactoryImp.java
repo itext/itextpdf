a30a7accf908ca9b559a24aefe5886a2c4c1ac81
Xavier Le Vourch <xavier@brittanysoftware.com>
2008-05-05 18:59:09 +0000

    new method registerDirectory() in FontFactoryImp to recursively register fonts.
    registerDirectories() uses the recursive scans on Unix paths and checks extra
    directories.

--- src/core/com/lowagie/text/FontFactoryImp.java
+++ src/core/com/lowagie/text/FontFactoryImp.java
@@ -599,6 +599,17 @@ public class FontFactoryImp {
      * @return the number of fonts registered
      */    
     public int registerDirectory(String dir) {
+        return registerDirectory(dir, false);
+    }
+
+    /**
+     * Register all the fonts in a directory and possibly its subdirectories.
+     * @param dir the directory
+     * @param scanSubdirectories recursively scan subdirectories if <code>true</true>
+     * @return the number of fonts registered
+     * @since 2.1.2
+     */
+    public int registerDirectory(String dir, boolean scanSubdirectories) {
         int count = 0;
         try {
             File file = new File(dir);
@@ -610,10 +621,24 @@ public class FontFactoryImp {
             for (int k = 0; k < files.length; ++k) {
                 try {
                     file = new File(dir, files[k]);
-                    String name = file.getPath().toLowerCase();
-                    if (name.endsWith(".ttf") || name.endsWith(".otf") || name.endsWith(".afm") || name.endsWith(".pfm") || name.endsWith(".ttc")) {
-                        register(file.getPath(), null);
-                        ++count;
+                    if (file.isDirectory()) {
+                        if (scanSubdirectories) {
+                            count += registerDirectory(file.getAbsolutePath(), true);
+                        }
+                    } else {
+                        String name = file.getPath();
+                        String suffix = name.length() < 4 ? null : name.substring(name.length() - 4).toLowerCase();
+                        if (".afm".equals(suffix) || ".pfm".equals(suffix)) {
+                            /* Only register Type 1 fonts with matching .pfb files */
+                            File pfb = new File(name.substring(0, name.length() - 4) + ".pfb");
+                            if (pfb.exists()) {
+                                register(name, null);
+                                ++count;
+                            }
+                        } else if (".ttf".equals(suffix) || ".otf".equals(suffix) || ".ttc".equals(suffix)) {
+                            register(name, null);
+                            ++count;
+                        }
                     }
                 }
                 catch (Exception e) {
@@ -637,10 +662,11 @@ public class FontFactoryImp {
         count += registerDirectory("c:/winnt/fonts");
         count += registerDirectory("d:/windows/fonts");
         count += registerDirectory("d:/winnt/fonts");
-        count += registerDirectory("/usr/X/lib/X11/fonts/TrueType");
-        count += registerDirectory("/usr/openwin/lib/X11/fonts/TrueType");
-        count += registerDirectory("/usr/share/fonts/default/TrueType");
-        count += registerDirectory("/usr/X11R6/lib/X11/fonts/ttf");
+        count += registerDirectory("/usr/share/X11/fonts", true);
+        count += registerDirectory("/usr/X/lib/X11/fonts", true);
+        count += registerDirectory("/usr/openwin/lib/X11/fonts", true);
+        count += registerDirectory("/usr/share/fonts", true);
+        count += registerDirectory("/usr/X11R6/lib/X11/fonts", true);
         count += registerDirectory("/Library/Fonts");
         count += registerDirectory("/System/Library/Fonts");
         return count;
