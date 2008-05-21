aa60c158dc0a998328b369bcc3f3c59443f2bc14
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2008-05-21 17:28:01 +0000

    I'm moving the examples out of the main branch

--- examples/resources/classroom/sqlscripts/film_category.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO film_category (category_id, name, keyword) VALUES (0, 'Unknown', '');
-INSERT INTO film_category (category_id, name, keyword) VALUES (1, 'Official selection (in competition)', 'comp');
-INSERT INTO film_category (category_id, name, keyword) VALUES (2, 'Official selection (preview)', 'prev');
-INSERT INTO film_category (category_id, name, keyword) VALUES (3, 'World cinema', 'world');
-INSERT INTO film_category (category_id, name, keyword) VALUES (4, 'History of film', 'hist');
-INSERT INTO film_category (category_id, name, keyword) VALUES (5, 'A Look Apart', 'apart');
-INSERT INTO film_category (category_id, name, keyword) VALUES (6, 'Focus', 'focus');
