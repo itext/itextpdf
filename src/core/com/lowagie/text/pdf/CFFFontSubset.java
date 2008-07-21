4ad49803bb75e4f2f9d02112b05440abc02b10e5
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2008-07-21 15:08:04 +0000

    This update by Ulrich Telle solves a problem when subsetting a CFF font (only blanks were displayed when opening the document with Adobe Reader 8 or higher).

--- src/core/com/lowagie/text/pdf/CFFFontSubset.java
+++ src/core/com/lowagie/text/pdf/CFFFontSubset.java
@@ -88,6 +88,12 @@ public class CFFFontSubset extends CFFFont {
 	};
 	
 	/**
+	*  Operator codes for unused  CharStrings and unused local and global Subrs
+	*/
+	static final byte ENDCHAR_OP = 14;
+	static final byte RETURN_OP = 11;
+	
+	/**
 	 * A HashMap containing the glyphs used in the text after being converted
 	 * to glyph number by the CMap 
 	 */
@@ -416,7 +422,7 @@ public class CFFFontSubset extends CFFFont {
 	 */
 	protected void BuildNewCharString(int FontIndex) throws IOException 
 	{
-		NewCharStringsIndex = BuildNewIndex(fonts[FontIndex].charstringsOffsets,GlyphsUsed);
+		NewCharStringsIndex = BuildNewIndex(fonts[FontIndex].charstringsOffsets,GlyphsUsed,ENDCHAR_OP);
 	}
 	
 	/**
@@ -461,7 +467,7 @@ public class CFFFontSubset extends CFFFont {
 					// by the glyphs. Scans the Subrs recursively. 
 					BuildSubrUsed(Font,FD,fonts[Font].PrivateSubrsOffset[FD],fonts[Font].PrivateSubrsOffsetsArray[FD],hSubrsUsed[FD],lSubrsUsed[FD]);
 					// Builds the New Local Subrs index
-					NewLSubrsIndex[FD] = BuildNewIndex(fonts[Font].PrivateSubrsOffsetsArray[FD],hSubrsUsed[FD]);
+					NewLSubrsIndex[FD] = BuildNewIndex(fonts[Font].PrivateSubrsOffsetsArray[FD],hSubrsUsed[FD],RETURN_OP);
 				}
 			}
 		}
@@ -479,9 +485,9 @@ public class CFFFontSubset extends CFFFont {
 		BuildGSubrsUsed(Font);
 		if (fonts[Font].privateSubrs>=0)
 			// Builds the New Local Subrs index
-			NewSubrsIndexNonCID = BuildNewIndex(fonts[Font].SubrsOffsets,hSubrsUsedNonCID);
+			NewSubrsIndexNonCID = BuildNewIndex(fonts[Font].SubrsOffsets,hSubrsUsedNonCID,RETURN_OP);
 		//Builds the New Global Subrs index
-		NewGSubrsIndex = BuildNewIndex(gsubrOffsets,hGSubrsUsed);
+		NewGSubrsIndex = BuildNewIndex(gsubrOffsets,hGSubrsUsed,RETURN_OP);
 	}
 
 	/**
@@ -923,11 +929,13 @@ public class CFFFontSubset extends CFFFont {
 	 * used for creating the glyph and subrs subsetted index 
 	 * @param Offsets the offset array of the original index  
 	 * @param Used the hashmap of the used objects
+	 * @param OperatorForUnusedEntries the operator inserted into the data stream for unused entries
 	 * @return the new index subset version 
 	 * @throws IOException
 	 */
-	protected byte[] BuildNewIndex(int[] Offsets,HashMap Used) throws IOException 
+	protected byte[] BuildNewIndex(int[] Offsets,HashMap Used,byte OperatorForUnusedEntries) throws IOException 
 	{
+		int unusedCount = 0;
 		int Offset=0;
 		int[] NewOffsets = new int[Offsets.length];
 		// Build the Offsets Array for the Subset
@@ -936,17 +944,22 @@ public class CFFFontSubset extends CFFFont {
 			NewOffsets[i] = Offset;
 			// If the object in the offset is also present in the used
 			// HashMap then increment the offset var by its size
-			if (Used.containsKey(new Integer(i)))
+			if (Used.containsKey(new Integer(i))) {
 				Offset += Offsets[i+1] - Offsets[i];
+			} else {
 				// Else the same offset is kept in i+1.
+				unusedCount++;
+			}
 		}
 		// Offset var determines the size of the object array
-		byte[] NewObjects = new byte[Offset];
+		byte[] NewObjects = new byte[Offset+unusedCount];
 		// Build the new Object array
+		int unusedOffset = 0;
 		for (int i=0;i<Offsets.length-1;++i)
 		{
 			int start = NewOffsets[i];
 			int end = NewOffsets[i+1];
+			NewOffsets[i] = start+unusedOffset;
 			// If start != End then the Object is used
 			// So, we will copy the object data from the font file
 			if (start != end)
@@ -955,9 +968,13 @@ public class CFFFontSubset extends CFFFont {
 				// Jump the file pointer to the start address to read from.
 				buf.seek(Offsets[i]);
 				// Read from the buffer and write into the array at start.  
-                buf.readFully(NewObjects, start, end-start);
+                buf.readFully(NewObjects, start+unusedOffset, end-start);
+			} else {
+				NewObjects[start+unusedOffset] = OperatorForUnusedEntries;
+				unusedOffset++;
 			}
 		}
+		NewOffsets[Offsets.length-1] += unusedOffset;
 		// Use AssembleIndex to build the index from the offset & object arrays
 		return AssembleIndex(NewOffsets,NewObjects);
 	}
