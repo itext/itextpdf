9203864ec61193a9e84457b40c848951046ca2f9
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2009-03-06 16:07:16 +0000

    Javadoc clean-up (Bernd Oliver Sünderhauf #2668047)
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@3760 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/core/com/lowagie/text/pdf/PdfObject.java
+++ itext/src/core/com/lowagie/text/pdf/PdfObject.java
@@ -70,93 +70,93 @@ import java.io.OutputStream;
  * @see		PdfStream
  * @see		PdfIndirectReference
  */
-
 public abstract class PdfObject {
 
-    // static membervariables (all the possible types of a PdfObject)
+    // CONSTANTS
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int BOOLEAN = 1;
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int NUMBER = 2;
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int STRING = 3;
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int NAME = 4;
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int ARRAY = 5;
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int DICTIONARY = 6;
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int STREAM = 7;
 
-/** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int NULL = 8;
 
-    /** a possible type of <CODE>PdfObject</CODE> */
+    /** A possible type of <CODE>PdfObject</CODE> */
     public static final int INDIRECT = 10;
 
-/** This is an empty string used for the <CODE>PdfNull</CODE>-object and for an empty <CODE>PdfString</CODE>-object. */
+    /** An empty string used for the <CODE>PdfNull</CODE>-object and for an empty <CODE>PdfString</CODE>-object. */
     public static final String NOTHING = "";
 
-/** This is the default encoding to be used for converting Strings into bytes and vice versa.
- * The default encoding is PdfDocEncoding.
- */
+    /**
+     * This is the default encoding to be used for converting Strings into
+     * bytes and vice versa. The default encoding is PdfDocEncoding.
+     */
     public static final String TEXT_PDFDOCENCODING = "PDF";
 
-/** This is the encoding to be used to output text in Unicode. */
+    /** This is the encoding to be used to output text in Unicode. */
     public static final String TEXT_UNICODE = "UnicodeBig";
 
-    // membervariables
+    // CLASS VARIABLES
 
-/** the content of this <CODE>PdfObject</CODE> */
+    /** The content of this <CODE>PdfObject</CODE> */
     protected byte[] bytes;
 
-/** the type of this <CODE>PdfObject</CODE> */
+    /** The type of this <CODE>PdfObject</CODE> */
     protected int type;
 
-    /**
-     * Holds value of property indRef.
-     */
+    /** Holds the indirect reference. */
     protected PdfIndirectReference indRef;
 
-    // constructors
+    // CONSTRUCTORS
 
-/**
- * Constructs a <CODE>PdfObject</CODE> of a certain <VAR>type</VAR> without any <VAR>content</VAR>.
- *
- * @param		type			type of the new <CODE>PdfObject</CODE>
- */
-
+    /**
+     * Constructs a <CODE>PdfObject</CODE> of a certain <VAR>type</VAR>
+     * without any <VAR>content</VAR>.
+     *
+     * @param type    type of the new <CODE>PdfObject</CODE>
+     */
     protected PdfObject(int type) {
         this.type = type;
     }
 
-/**
- * Constructs a <CODE>PdfObject</CODE> of a certain <VAR>type</VAR> with a certain <VAR>content</VAR>.
- *
- * @param		type			type of the new <CODE>PdfObject</CODE>
- * @param		content			content of the new <CODE>PdfObject</CODE> as a <CODE>String</CODE>.
- */
-
+    /**
+     * Constructs a <CODE>PdfObject</CODE> of a certain <VAR>type</VAR>
+     * with a certain <VAR>content</VAR>.
+     *
+     * @param type     type of the new <CODE>PdfObject</CODE>
+     * @param content  content of the new <CODE>PdfObject</CODE> as a
+     *   <CODE>String</CODE>.
+     */
     protected PdfObject(int type, String content) {
         this.type = type;
         bytes = PdfEncodings.convertToBytes(content, null);
     }
 
-/**
- * Constructs a <CODE>PdfObject</CODE> of a certain <VAR>type</VAR> with a certain <VAR>content</VAR>.
- *
- * @param		type			type of the new <CODE>PdfObject</CODE>
- * @param		bytes			content of the new <CODE>PdfObject</CODE> as an array of <CODE>byte</CODE>.
- */
-
+    /**
+     * Constructs a <CODE>PdfObject</CODE> of a certain <VAR>type</VAR>
+     * with a certain <VAR>content</VAR>.
+     *
+     * @param type   type of the new <CODE>PdfObject</CODE>
+     * @param bytes  content of the new <CODE>PdfObject</CODE> as an array of
+     *   <CODE>byte</CODE>.
+     */
     protected PdfObject(int type, byte[] bytes) {
         this.bytes = bytes;
         this.type = type;
@@ -164,20 +164,34 @@ public abstract class PdfObject {
 
     // methods dealing with the content of this object
 
-/**
- * Writes the PDF representation of this <CODE>PdfObject</CODE> as an array of <CODE>byte</CODE>s to the writer.
- * @param writer for backwards compatibility
- * @param os the outputstream to write the bytes to.
- * @throws IOException
- */
-
+    /**
+     * Writes the PDF representation of this <CODE>PdfObject</CODE> as an
+     * array of <CODE>byte</CODE>s to the writer.
+     * 
+     * @param writer for backwards compatibility
+     * @param os     The <CODE>OutputStream</CODE> to write the bytes to.
+     * @throws IOException
+     */
     public void toPdf(PdfWriter writer, OutputStream os) throws IOException {
         if (bytes != null)
             os.write(bytes);
     }
 
     /**
+     * Returns the <CODE>String</CODE>-representation of this
+     * <CODE>PdfObject</CODE>.
+     *
+     * @return    a <CODE>String</CODE>
+     */
+    public String toString() {
+        if (bytes == null)
+            return super.toString();
+        return PdfEncodings.convertToString(bytes, null);
+    }
+
+    /**
      * Gets the presentation of this object in a byte array
+     * 
      * @return a byte array
      */
     public byte[] getBytes() {
@@ -185,190 +199,208 @@ public abstract class PdfObject {
     }
 
     /**
-     * Can this object be in an object stream?
-     * @return true if this object can be in an object stream.
+     * Whether this object can be contained in an object stream.
+     * 
+     * PdfObjects of type STREAM OR INDIRECT can not be contained in an
+     * object stream.
+     * 
+     * @return <CODE>true</CODE> if this object can be in an object stream.
+     *   Otherwise <CODE>false</CODE>
      */
     public boolean canBeInObjStm() {
-        return (type >= 1 && type <= 6) || type == 8;
+        switch (type) {
+            case NULL:
+            case BOOLEAN:
+            case NUMBER:
+            case STRING:
+            case NAME:
+            case ARRAY:
+            case DICTIONARY:
+                return true;
+            case STREAM:
+            case INDIRECT:
+            default:
+                return false;
+        }
     }
 
-/**
- * Returns the length of the PDF representation of the <CODE>PdfObject</CODE>.
- * <P>
- * In some cases, namely for <CODE>PdfString</CODE> and <CODE>PdfStream</CODE>,
- * this method differs from the method <CODE>length</CODE> because <CODE>length</CODE>
- * returns the length of the actual content of the <CODE>PdfObject</CODE>.</P>
- * <P>
- * Remark: the actual content of an object is in most cases identical to its representation.
- * The following statement is always true: length() &gt;= pdfLength().</P>
- *
- * @return		a length
- */
-
+    /**
+     * Returns the length of the PDF representation of the <CODE>PdfObject</CODE>.
+     * <P>
+     * In some cases, namely for <CODE>PdfString</CODE> and <CODE>PdfStream</CODE>,
+     * this method differs from the method <CODE>length</CODE> because <CODE>length</CODE>
+     * returns the length of the actual content of the <CODE>PdfObject</CODE>.</P>
+     * <P>
+     * Remark: the actual content of an object is in most cases identical to its representation.
+     * The following statement is always true: length() &gt;= pdfLength().</P>
+     *
+     * @return		a length
+     */
 //    public int pdfLength() {
 //        return toPdf(null).length;
 //    }
 
-/**
- * Returns the <CODE>String</CODE>-representation of this <CODE>PdfObject</CODE>.
- *
- * @return		a <CODE>String</CODE>
- */
-
-    public String toString() {
-        if (bytes == null)
-            return super.toString();
-        else
-            return PdfEncodings.convertToString(bytes, null);
-    }
-
-/**
- * Returns the length of the actual content of the <CODE>PdfObject</CODE>.
- * <P>
- * In some cases, namely for <CODE>PdfString</CODE> and <CODE>PdfStream</CODE>,
- * this method differs from the method <CODE>pdfLength</CODE> because <CODE>pdfLength</CODE>
- * returns the length of the PDF representation of the object, not of the actual content
- * as does the method <CODE>length</CODE>.</P>
- * <P>
- * Remark: the actual content of an object is in some cases identical to its representation.
- * The following statement is always true: length() &gt;= pdfLength().</P>
- *
- * @return		a length
- */
-
+    /**
+     * Returns the length of the actual content of the <CODE>PdfObject</CODE>.
+     * <P>
+     * In some cases, namely for <CODE>PdfString</CODE> and <CODE>PdfStream</CODE>,
+     * this method differs from the method <CODE>pdfLength</CODE> because <CODE>pdfLength</CODE>
+     * returns the length of the PDF representation of the object, not of the actual content
+     * as does the method <CODE>length</CODE>.</P>
+     * <P>
+     * Remark: the actual content of an object is in some cases identical to its representation.
+     * The following statement is always true: length() &gt;= pdfLength().</P>
+     *
+     * @return The length as <CODE>int</CODE>
+     */
     public int length() {
         return toString().length();
     }
 
-/**
- * Changes the content of this <CODE>PdfObject</CODE>.
- *
- * @param		content			the new content of this <CODE>PdfObject</CODE>
- */
-
+    /**
+     * Changes the content of this <CODE>PdfObject</CODE>.
+     *
+     * @param content    the new content of this <CODE>PdfObject</CODE>
+     */
     protected void setContent(String content) {
         bytes = PdfEncodings.convertToBytes(content, null);
     }
 
     // methods dealing with the type of this object
 
-/**
- * Returns the type of this <CODE>PdfObject</CODE>.
- *
- * @return		a type
- */
-
+    /**
+     * Returns the type of this <CODE>PdfObject</CODE>.
+     * 
+     * May be either of:
+     * - <VAR>NULL</VAR>: A <CODE>PdfNull</CODE>
+     * - <VAR>BOOLEAN</VAR>: A <CODE>PdfBoolean</CODE>
+     * - <VAR>NUMBER</VAR>: A <CODE>PdfNumber</CODE>
+     * - <VAR>STRING</VAR>: A <CODE>PdfString</CODE>
+     * - <VAR>NAME</VAR>: A <CODE>PdfName</CODE>
+     * - <VAR>ARRAY</VAR>: A <CODE>PdfArray</CODE>
+     * - <VAR>DICTIONARY</VAR>: A <CODE>PdfDictionary</CODE>
+     * - <VAR>STREAM</VAR>: A <CODE>PdfStream</CODE>
+     * - <VAR>INDIRECT</VAR>: ><CODE>PdfIndirectObject</CODE>
+     *
+     * @return The type
+     */
     public int type() {
         return type;
     }
 
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfNull</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfNull</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
     public boolean isNull() {
-        return (this.type == NULL);
-    }
-
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfBoolean</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
-    public boolean isBoolean() {
-        return (this.type == BOOLEAN);
-    }
-
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfNumber</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
-    public boolean isNumber() {
-        return (this.type == NUMBER);
-    }
-
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfString</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
-    public boolean isString() {
-        return (this.type == STRING);
-    }
-
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfName</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
-    public boolean isName() {
-        return (this.type == NAME);
-    }
-
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfArray</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
-    public boolean isArray() {
-        return (this.type == ARRAY);
-    }
-
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfDictionary</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
-    public boolean isDictionary() {
-        return (this.type == DICTIONARY);
-    }
-
-/**
- * Checks if this <CODE>PdfObject</CODE> is of the type <CODE>PdfStream</CODE>.
- *
- * @return		<CODE>true</CODE> or <CODE>false</CODE>
- */
-
-    public boolean isStream() {
-        return (this.type == STREAM);
+        return (type == NULL);
     }
 
     /**
-     * Checks if this is an indirect object.
-     * @return true if this is an indirect object
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfBoolean</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
+    public boolean isBoolean() {
+        return (type == BOOLEAN);
+    }
+
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfNumber</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
+    public boolean isNumber() {
+        return (type == NUMBER);
+    }
+
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfString</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
+    public boolean isString() {
+        return (type == STRING);
+    }
+
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfName</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
+    public boolean isName() {
+        return (type == NAME);
+    }
+
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfArray</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
+    public boolean isArray() {
+        return (type == ARRAY);
+    }
+
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfDictionary</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
+    public boolean isDictionary() {
+        return (type == DICTIONARY);
+    }
+
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfStream</CODE>.
+     *
+     * @return <CODE>true</CODE> or <CODE>false</CODE>
+     */
+    public boolean isStream() {
+        return (type == STREAM);
+    }
+
+    /**
+     * Checks if this <CODE>PdfObject</CODE> is of the type
+     * <CODE>PdfIndirectObject</CODE>.
+     * 
+     * @return <CODE>true</CODE> if this is an indirect object,
+     *   otherwise <CODE>false</CODE>
      */
     public boolean isIndirect() {
-        return (this.type == INDIRECT);
+        return (type == INDIRECT);
     }
 
     /**
-     * Getter for property indRef.
-     * @return Value of property indRef.
+     * Get the indirect reference
+     * 
+     * @return A <CODE>PdfIndirectReference</CODE>
      */
     public PdfIndirectReference getIndRef() {
         return indRef;
     }
 
     /**
-     * Setter for property indRef.
-     * @param indRef New value of property indRef.
+     * Set the indirect reference
+     * 
+     * @param indRef New value as a <CODE>PdfIndirectReference</CODE>
      */
     public void setIndRef(PdfIndirectReference indRef) {
         this.indRef = indRef;
-        indRef.setDirectObject( this );
+        indRef.setDirectObject(this);
     }
 
     /**
-     *  Simply here to be overridden by indirect references.
+     * Simply here to be overridden by indirect references.
+     * 
      * @return this
      * @since 2.1.5
      */
