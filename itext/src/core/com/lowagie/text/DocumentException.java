0613326ae0ee46253916b51ad26a2933b165fadd
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2009-04-01 14:58:04 +0000

    Added a getEx() method to DocumentException (Feature request #2691769)
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@3828 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/core/com/lowagie/text/DocumentException.java
+++ itext/src/core/com/lowagie/text/DocumentException.java
@@ -60,15 +60,16 @@ package com.lowagie.text;
  */
 
 public class DocumentException extends Exception {
+	
+	/** A serial version UID */
     private static final long serialVersionUID = -2191131489390840739L;
-	private Exception ex;
 
     /**
      * Creates a Document exception.
      * @param ex an exception that has to be turned into a DocumentException
      */
     public DocumentException(Exception ex) {
-        this.ex = ex;
+        super(ex);
     }
     
     // constructors
@@ -87,87 +88,5 @@ public class DocumentException extends Exception {
      */
     public DocumentException(String message) {
         super(message);
-    }
-
-    /**
-     * We print the message of the checked exception 
-     * @return the error message
-     */
-    public String getMessage() {
-        if (ex == null)
-            return super.getMessage();
-        else
-            return ex.getMessage();
-    }
-
-    /**
-     * and make sure we also produce a localized version 
-     * @return a localized message
-     */
-    public String getLocalizedMessage() {
-        if (ex == null)
-            return super.getLocalizedMessage();
-        else
-            return ex.getLocalizedMessage();
-    }
-
-    /**
-     * The toString() is changed to be prefixed with ExceptionConverter 
-     * @return the String version of the exception
-     */
-    public String toString() {
-        if (ex == null)
-            return super.toString();
-        else
-            return split(getClass().getName()) + ": " + ex;
-    }
-
-    /** we have to override this as well */
-    public void printStackTrace() {
-        printStackTrace(System.err);
-    }
-
-    /**
-     * here we prefix, with s.print(), not s.println(), the stack
-     * trace with "ExceptionConverter:" 
-     * @param s a printstream object
-     */
-    public void printStackTrace(java.io.PrintStream s) {
-        if (ex == null)
-            super.printStackTrace(s);
-        else {
-            synchronized (s) {
-                s.print(split(getClass().getName()) + ": ");
-                ex.printStackTrace(s);
-            }
-        }
-    }
-
-    /**
-     * Again, we prefix the stack trace with "ExceptionConverter:" 
-     * @param s A PrintWriter object
-     */
-    public void printStackTrace(java.io.PrintWriter s) {
-        if (ex == null)
-            super.printStackTrace(s);
-        else {
-            synchronized (s) {
-                s.print(split(getClass().getName()) + ": ");
-                ex.printStackTrace(s);
-            }
-        }
-    }
-
-    /**
-     * Removes everything in a String that comes before a '.'
-     * @param s the original string
-     * @return the part that comes after the dot
-     */
-    private static String split(String s) {
-        int i = s.lastIndexOf('.');
-        if (i < 0)
-            return s;
-        else
-            return s.substring(i + 1);
     }
 }
