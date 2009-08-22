0325b616eefb778da79378c59b4be3d54a914d90
Xavier Le Vourch <xavier@brittanysoftware.com>
2009-08-22 17:34:38 +0000

    code clean up (PMD warning): class has no public constructor -> final
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@4051 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/core/com/lowagie/text/error_messages/MessageLocalization.java
+++ itext/src/core/com/lowagie/text/error_messages/MessageLocalization.java
@@ -63,7 +63,7 @@ import java.util.HashMap;
  * normal property file. See en.lng for more information on the internal format.
  * @author Paulo Soares (psoares@glintt.com)
  */
-public class MessageLocalization {
+public final class MessageLocalization {
     private static HashMap defaultLanguage = new HashMap();
     private static HashMap currentLanguage;
     private static final String BASE_PATH = "com/lowagie/text/error_messages/";
