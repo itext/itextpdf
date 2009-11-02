5818dd3d1b595be1d371373e0cc963143c0b3601
Kevin Day <kevin@trumpetinc.com>
2009-11-02 16:57:09 +0000

    Refactoring to allow different text extraction implementations

--- src/core/com/lowagie/text/pdf/parser/RenderListener.java
+++ src/core/com/lowagie/text/pdf/parser/RenderListener.java
@@ -1,17 +1,15 @@
 /*
- * Created on Oct 6, 2009
+ * Created on Oct 28, 2009
  * (c) 2009 Trumpet, Inc.
  *
  */
 package com.lowagie.text.pdf.parser;
 
-
 /**
- * Defines the callback method(s) that {@link PdfContentStreamProcessor} will make as
- * various render operations are encountered during processing of a context stream
- * @since 2.1.5
+ * @author kevin
  */
-public abstract class RenderListener {
+public interface RenderListener {
+
     /**
      * Called when text should be rendered
      * @param text the text to render
@@ -19,5 +17,11 @@ public abstract class RenderListener {
      * @param textMatrix the text matrix at the beginning of the operation
      * @param endingTextMatrix the text matrix that will exist at the end of the operation.  This is useful for determining the width of the string without having to re-compute it.
      */
-    abstract public void renderText(String text, GraphicsState gs, Matrix textMatrix, Matrix endingTextMatrix); 
+    public void renderText(String text, GraphicsState gs, Matrix textMatrix,
+            Matrix endingTextMatrix);
+
+    /**
+     * Resets the internal state of the RenderListener
+     */
+    public void reset();
 }
