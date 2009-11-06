076ce8e8b97219835f6512baa29a7c31584759d4
Kevin Day <kevin@trumpetinc.com>
2009-11-06 21:11:21 +0000

    Added ability to get content byte from non-stream objects (thanks to Ken Dobson for ideas on how to do this from submitted unit test)

--- src/core/com/lowagie/text/pdf/parser/PdfTextExtractor.java
+++ src/core/com/lowagie/text/pdf/parser/PdfTextExtractor.java
@@ -46,10 +46,16 @@
  */
 package com.lowagie.text.pdf.parser;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ListIterator;
 
+import com.lowagie.text.pdf.PRIndirectReference;
+import com.lowagie.text.pdf.PRStream;
+import com.lowagie.text.pdf.PdfArray;
 import com.lowagie.text.pdf.PdfDictionary;
 import com.lowagie.text.pdf.PdfName;
+import com.lowagie.text.pdf.PdfObject;
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.RandomAccessFileOrArray;
 
@@ -84,17 +90,62 @@ public class PdfTextExtractor {
     }
     
     /**
-     * Gets the content stream of a page.
+     * Gets the content bytes of a page.
      * @param pageNum	the page number of page you want get the content stream from
-     * @return	a byte array with the content stream of a page
+     * @return	a byte array with the effective content stream of a page
      * @throws IOException
      */
     private byte[] getContentBytesForPage(int pageNum) throws IOException {
         RandomAccessFileOrArray f = reader.getSafeFile();
-        byte[] contentBytes = reader.getPageContent(pageNum, f);
-        f.close();
-        return contentBytes;
+        try{
+            final PdfDictionary pageDictionary = reader.getPageN(pageNum);
+            final PdfObject contentObject = pageDictionary.get(PdfName.CONTENTS);
+            final byte[] contentBytes = getContentBytesFromContentObject(contentObject);
+            return contentBytes;
+        } finally {    
+            f.close();
+        }
     }
+    
+    /**
+     * Gets the content bytes from a content object, which may be a reference
+     * a stream or an array.
+     * @param contentObject the object to read bytes from
+     * @return the content bytes
+     * @throws IOException
+     */
+    private byte[] getContentBytesFromContentObject(final PdfObject contentObject) throws IOException {
+          final byte[] result;
+          switch (contentObject.type())
+          {
+            case PdfObject.INDIRECT:
+              final PRIndirectReference ref = (PRIndirectReference) contentObject;
+              final PdfObject directObject = PdfReader.getPdfObject(ref);
+              result = getContentBytesFromContentObject(directObject);
+              break;
+            case PdfObject.STREAM:
+              final PRStream stream = (PRStream) PdfReader.getPdfObject(contentObject);
+              result = PdfReader.getStreamBytes(stream);
+              break;
+            case PdfObject.ARRAY:
+              // Stitch together all content before calling processContent(), because
+              // processContent() resets state.
+              final ByteArrayOutputStream allBytes = new ByteArrayOutputStream();
+              final PdfArray contentArray = (PdfArray) contentObject;
+              final ListIterator iter = contentArray.listIterator();
+              while (iter.hasNext())
+              {
+                final PdfObject element = (PdfObject) iter.next();
+                allBytes.write(getContentBytesFromContentObject(element));
+              }
+              result = allBytes.toByteArray();
+              break;
+            default:
+              final String msg = "Unable to handle Content of type " + contentObject.getClass();
+              throw new IllegalStateException(msg);
+          }
+          return result;
+        }    
     
     /**
      * Gets the text from a page.
