94c116b5aecd9b2d8611af63c6aa899622726db5
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2010-03-23 14:43:38 +0000

    Forgot to escape the XML.

--- src/core/com/itextpdf/text/pdf/parser/TaggedPdfReaderTool.java
+++ src/core/com/itextpdf/text/pdf/parser/TaggedPdfReaderTool.java
@@ -54,120 +54,145 @@ import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfNumber;
 import com.itextpdf.text.pdf.PdfObject;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.xml.simpleparser.SimpleXMLParser;
 
 /**
  * Converts a tagged PDF document into an XML file.
+ * 
  * @since 5.0.2
  */
 public class TaggedPdfReaderTool {
 
-    /** The reader object from which the content streams are read. */
-    PdfReader reader;
-    /** The writer object to which the XML will be written */
-    PrintWriter out;
-	
-    /**
-     * Parses a string with structured content.
-     * @param filename the name of the PDF file
-     * @param xml the name of the resulting xml file
-     */
-    public void convertToXml(PdfReader reader, OutputStream os) throws IOException {
-        this.reader = reader;
-        out = new PrintWriter(os);
-        // get the StructTreeRoot from the root object
-        PdfDictionary catalog = reader.getCatalog();
-        PdfDictionary struct = catalog.getAsDict(PdfName.STRUCTTREEROOT);
-        // Inspect the child or children of the StructTreeRoot
-        inspectChild(struct.getDirectObject(PdfName.K));
-        out.flush();
-        out.close();
-    }
-	
+	/** The reader object from which the content streams are read. */
+	PdfReader reader;
+	/** The writer object to which the XML will be written */
+	PrintWriter out;
+
 	/**
-	 * Inspects a child of a structured element.
-	 * This can be an array or a dictionary.
-	 * @param k the child to inspect
+	 * Parses a string with structured content.
+	 * 
+	 * @param filename
+	 *            the name of the PDF file
+	 * @param xml
+	 *            the name of the resulting xml file
+	 */
+	public void convertToXml(PdfReader reader, OutputStream os)
+			throws IOException {
+		this.reader = reader;
+		out = new PrintWriter(os);
+		// get the StructTreeRoot from the root object
+		PdfDictionary catalog = reader.getCatalog();
+		PdfDictionary struct = catalog.getAsDict(PdfName.STRUCTTREEROOT);
+		// Inspect the child or children of the StructTreeRoot
+		inspectChild(struct.getDirectObject(PdfName.K));
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Inspects a child of a structured element. This can be an array or a
+	 * dictionary.
+	 * 
+	 * @param k
+	 *            the child to inspect
 	 * @throws IOException
 	 */
 	public void inspectChild(PdfObject k) throws IOException {
 		if (k == null)
 			return;
 		if (k instanceof PdfArray)
-			inspectChildArray((PdfArray)k);
+			inspectChildArray((PdfArray) k);
 		else if (k instanceof PdfDictionary)
-			inspectChildDictionary((PdfDictionary)k);
+			inspectChildDictionary((PdfDictionary) k);
 	}
 
-    /**
-     * If the child of a structured element is an array,
-     * we need to loop over the elements.
-     * @param k the child array to inspect
-     */
-    public void inspectChildArray(PdfArray k) throws IOException {
-        if (k == null) return;
-        for (int i = 0; i < k.size(); i++) {
-            inspectChild(k.getDirectObject(i));
-        }
-    }
-	
 	/**
-	 * If the child of a structured element is a dictionary,
-	 * we inspect the child; we may also draw a tag.
-     * @param k the child dictionary to inspect
+	 * If the child of a structured element is an array, we need to loop over
+	 * the elements.
+	 * 
+	 * @param k
+	 *            the child array to inspect
 	 */
-    public void inspectChildDictionary(PdfDictionary k)
-        throws IOException {
-        if (k == null) return;
-        PdfName s = k.getAsName(PdfName.S);
-        if (s != null) {
-            String tag = s.toString().substring(1);
-            out.print("<");
-            out.print(tag);
-            out.print(">");
-            PdfDictionary dict = k.getAsDict(PdfName.PG);
-            if (dict != null)
-                parseTag(tag, k.getDirectObject(PdfName.K), dict);
-            inspectChild(k.get(PdfName.K));
-            out.print("</");
-            out.print(tag);
-            out.println(">");
-        }
-        else
-            inspectChild(k.get(PdfName.K));
-    }
+	public void inspectChildArray(PdfArray k) throws IOException {
+		if (k == null)
+			return;
+		for (int i = 0; i < k.size(); i++) {
+			inspectChild(k.getDirectObject(i));
+		}
+	}
 
-    /**
-     * Searches for a tag in a page.
-     * @param tag the name of the tag
-     * @param object an identifier to find the marked content
-     * @param page a page dictionary
-     * @throws IOException
-     */
-    public void parseTag(String tag, PdfObject object, PdfDictionary page) throws IOException {
-    	PRStream stream = (PRStream) page.getAsStream(PdfName.CONTENTS);
+	/**
+	 * If the child of a structured element is a dictionary, we inspect the
+	 * child; we may also draw a tag.
+	 * 
+	 * @param k
+	 *            the child dictionary to inspect
+	 */
+	public void inspectChildDictionary(PdfDictionary k) throws IOException {
+		if (k == null)
+			return;
+		PdfName s = k.getAsName(PdfName.S);
+		if (s != null) {
+			String tag = s.toString().substring(1);
+			out.print("<");
+			out.print(tag);
+			out.print(">");
+			PdfDictionary dict = k.getAsDict(PdfName.PG);
+			if (dict != null)
+				parseTag(tag, k.getDirectObject(PdfName.K), dict);
+			inspectChild(k.get(PdfName.K));
+			out.print("</");
+			out.print(tag);
+			out.println(">");
+		} else
+			inspectChild(k.get(PdfName.K));
+	}
+
+	/**
+	 * Searches for a tag in a page.
+	 * 
+	 * @param tag
+	 *            the name of the tag
+	 * @param object
+	 *            an identifier to find the marked content
+	 * @param page
+	 *            a page dictionary
+	 * @throws IOException
+	 */
+	public void parseTag(String tag, PdfObject object, PdfDictionary page)
+			throws IOException {
+		PRStream stream = (PRStream) page.getAsStream(PdfName.CONTENTS);
 		// if the identifier is a number, we can extract the content right away
 		if (object instanceof PdfNumber) {
 			PdfNumber mcid = (PdfNumber) object;
-	        RenderFilter filter = new MarkedContentRenderFilter(mcid.intValue());
-	        TextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
-	        FilteredTextRenderListener listener = new FilteredTextRenderListener(strategy, filter);
-	        PdfContentStreamProcessor processor = new PdfContentStreamProcessor(listener);
-	        processor.processContent(PdfReader.getStreamBytes(stream), page.getAsDict(PdfName.RESOURCES));
-	        out.print(listener.getResultantText());
+			RenderFilter filter = new MarkedContentRenderFilter(mcid.intValue());
+			TextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+			FilteredTextRenderListener listener = new FilteredTextRenderListener(
+					strategy, filter);
+			PdfContentStreamProcessor processor = new PdfContentStreamProcessor(
+					listener);
+			processor.processContent(PdfReader.getStreamBytes(stream), page
+					.getAsDict(PdfName.RESOURCES));
+			out.print(SimpleXMLParser.escapeXML(listener.getResultantText(), true));
 		}
-		// if the identifier is an array, we call the parseTag method recursively
+		// if the identifier is an array, we call the parseTag method
+		// recursively
 		else if (object instanceof PdfArray) {
 			PdfArray arr = (PdfArray) object;
 			int n = arr.size();
 			for (int i = 0; i < n; i++) {
 				parseTag(tag, arr.getPdfObject(i), page);
-				if (i < n - 1) out.println();
+				if (i < n - 1)
+					out.println();
 			}
 		}
-		// if the identifier is a dictionary, we get the resources from the dictionary
+		// if the identifier is a dictionary, we get the resources from the
+		// dictionary
 		else if (object instanceof PdfDictionary) {
 			PdfDictionary mcr = (PdfDictionary) object;
-			parseTag(tag, mcr.getDirectObject(PdfName.MCID), mcr.getAsDict(PdfName.PG));
+			parseTag(tag, mcr.getDirectObject(PdfName.MCID), mcr
+					.getAsDict(PdfName.PG));
 		}
 	}
+
 }
