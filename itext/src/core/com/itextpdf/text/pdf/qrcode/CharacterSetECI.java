6694f456d889a87a6a325ec6747ed6abbc4445a5
Paulo Soares <pgpsoares@gmail.com>
2010-04-03 18:36:58 +0000

    Safe initialization in a multi-threaded environment.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@4440 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/core/com/itextpdf/text/pdf/qrcode/CharacterSetECI.java
+++ itext/src/core/com/itextpdf/text/pdf/qrcode/CharacterSetECI.java
@@ -24,75 +24,61 @@ import java.util.HashMap;
  *
  * @author Sean Owen
  */
-public final class CharacterSetECI extends ECI {
+public final class CharacterSetECI {
 
-  private static HashMap<Integer,CharacterSetECI> VALUE_TO_ECI;
   private static HashMap<String,CharacterSetECI> NAME_TO_ECI;
 
   private static void initialize() {
-    VALUE_TO_ECI = new HashMap<Integer, CharacterSetECI>(29);
-    NAME_TO_ECI = new HashMap<String, CharacterSetECI>(29);
+    HashMap<String,CharacterSetECI> n = new HashMap<String, CharacterSetECI>(29);
     // TODO figure out if these values are even right!
-    addCharacterSet(0, "Cp437");
-    addCharacterSet(1, new String[] {"ISO8859_1", "ISO-8859-1"});
-    addCharacterSet(2, "Cp437");
-    addCharacterSet(3, new String[] {"ISO8859_1", "ISO-8859-1"});
-    addCharacterSet(4, new String[] {"ISO8859_2", "ISO-8859-2"});
-    addCharacterSet(5, new String[] {"ISO8859_3", "ISO-8859-3"});
-    addCharacterSet(6, new String[] {"ISO8859_4", "ISO-8859-4"});
-    addCharacterSet(7, new String[] {"ISO8859_5", "ISO-8859-5"});
-    addCharacterSet(8, new String[] {"ISO8859_6", "ISO-8859-6"});
-    addCharacterSet(9, new String[] {"ISO8859_7", "ISO-8859-7"});
-    addCharacterSet(10, new String[] {"ISO8859_8", "ISO-8859-8"});
-    addCharacterSet(11, new String[] {"ISO8859_9", "ISO-8859-9"});
-    addCharacterSet(12, new String[] {"ISO8859_10", "ISO-8859-10"});
-    addCharacterSet(13, new String[] {"ISO8859_11", "ISO-8859-11"});
-    addCharacterSet(15, new String[] {"ISO8859_13", "ISO-8859-13"});
-    addCharacterSet(16, new String[] {"ISO8859_14", "ISO-8859-14"});
-    addCharacterSet(17, new String[] {"ISO8859_15", "ISO-8859-15"});
-    addCharacterSet(18, new String[] {"ISO8859_16", "ISO-8859-16"});
-    addCharacterSet(20, new String[] {"SJIS", "Shift_JIS"});
+    addCharacterSet(0, "Cp437", n);
+    addCharacterSet(1, new String[] {"ISO8859_1", "ISO-8859-1"}, n);
+    addCharacterSet(2, "Cp437", n);
+    addCharacterSet(3, new String[] {"ISO8859_1", "ISO-8859-1"}, n);
+    addCharacterSet(4, new String[] {"ISO8859_2", "ISO-8859-2"}, n);
+    addCharacterSet(5, new String[] {"ISO8859_3", "ISO-8859-3"}, n);
+    addCharacterSet(6, new String[] {"ISO8859_4", "ISO-8859-4"}, n);
+    addCharacterSet(7, new String[] {"ISO8859_5", "ISO-8859-5"}, n);
+    addCharacterSet(8, new String[] {"ISO8859_6", "ISO-8859-6"}, n);
+    addCharacterSet(9, new String[] {"ISO8859_7", "ISO-8859-7"}, n);
+    addCharacterSet(10, new String[] {"ISO8859_8", "ISO-8859-8"}, n);
+    addCharacterSet(11, new String[] {"ISO8859_9", "ISO-8859-9"}, n);
+    addCharacterSet(12, new String[] {"ISO8859_10", "ISO-8859-10"}, n);
+    addCharacterSet(13, new String[] {"ISO8859_11", "ISO-8859-11"}, n);
+    addCharacterSet(15, new String[] {"ISO8859_13", "ISO-8859-13"}, n);
+    addCharacterSet(16, new String[] {"ISO8859_14", "ISO-8859-14"}, n);
+    addCharacterSet(17, new String[] {"ISO8859_15", "ISO-8859-15"}, n);
+    addCharacterSet(18, new String[] {"ISO8859_16", "ISO-8859-16"}, n);
+    addCharacterSet(20, new String[] {"SJIS", "Shift_JIS"}, n);
+    NAME_TO_ECI = n;
   }
 
   private final String encodingName;
+  private final int value;
 
   private CharacterSetECI(int value, String encodingName) {
-    super(value);
     this.encodingName = encodingName;
+    this.value = value;
   }
 
   public String getEncodingName() {
     return encodingName;
   }
 
-  private static void addCharacterSet(int value, String encodingName) {
+  public int getValue() {
+    return value;
+  }
+
+  private static void addCharacterSet(int value, String encodingName, HashMap<String,CharacterSetECI> n) {
     CharacterSetECI eci = new CharacterSetECI(value, encodingName);
-    VALUE_TO_ECI.put(new Integer(value), eci); // can't use valueOf
-    NAME_TO_ECI.put(encodingName, eci);
+    n.put(encodingName, eci);
   }
 
-  private static void addCharacterSet(int value, String[] encodingNames) {
+  private static void addCharacterSet(int value, String[] encodingNames, HashMap<String,CharacterSetECI> n) {
     CharacterSetECI eci = new CharacterSetECI(value, encodingNames[0]);
-    VALUE_TO_ECI.put(new Integer(value), eci); // can't use valueOf
     for (int i = 0; i < encodingNames.length; i++) {
-      NAME_TO_ECI.put(encodingNames[i], eci);
+      n.put(encodingNames[i], eci);
     }
-  }
-
-  /**
-   * @param value character set ECI value
-   * @return {@link CharacterSetECI} representing ECI of given value, or null if it is legal but
-   *   unsupported
-   * @throws IllegalArgumentException if ECI value is invalid
-   */
-  public static CharacterSetECI getCharacterSetECIByValue(int value) {
-    if (VALUE_TO_ECI == null) {
-      initialize();
-    }
-    if (value < 0 || value >= 900) {
-      throw new IllegalArgumentException("Bad ECI value: " + value);
-    }
-    return VALUE_TO_ECI.get(new Integer(value));
   }
 
   /**
