7348e48cab25fd71842df7e7af6f47dee7df5c5c
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2010-04-05 06:57:29 +0000

    Missing since tag.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@4445 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/core/com/itextpdf/text/pdf/collection/PdfCollection.java
+++ itext/src/core/com/itextpdf/text/pdf/collection/PdfCollection.java
@@ -55,7 +55,10 @@ public class PdfCollection extends PdfDictionary {
 	public static final int TILE = 1;
 	/** A type of PDF Collection */
 	public static final int HIDDEN = 2;
-	/** A type of PDF Collection */
+	/**
+	 * A type of PDF Collection
+	 * @since 5.0.2
+	 */
 	public static final int CUSTOM = 3;
 	
 	/**
