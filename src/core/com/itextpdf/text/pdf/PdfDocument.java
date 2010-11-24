c1f7880357dc8e31e6618f2ff93bdb6e49ecf493
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2010-11-24 15:08:13 +0000

    Fixed bug #3114043: when content really doesn't fit a row, iText should throw an Exception instead of printing the error message to the PDF

--- src/core/com/itextpdf/text/pdf/PdfDocument.java
+++ src/core/com/itextpdf/text/pdf/PdfDocument.java
@@ -2398,8 +2398,7 @@ public class PdfDocument extends Document {
             else
                 loop = 0;
             if (loop == 3) {
-                add(new Paragraph("ERROR: Infinite table loop"));
-                break;
+            	throw new DocumentException(MessageLocalization.getComposedMessage("infinite.table.loop"));
             }
             newPage();
         }
