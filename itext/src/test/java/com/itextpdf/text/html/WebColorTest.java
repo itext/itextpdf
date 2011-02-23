c5cdb3177b5ebe57c652ad2e363817ae6d67c332
Balder Van Camp <balder@redlab.be>
2011-02-23 13:30:36 +0000

    Bugfix of incorrect implementation of WebColors => The three-digit RGB notation (#rgb) is converted into six-digit form (#rrggbb) by replicating digits, not by adding zeros. For example, #fb0 expands to #ffbb00. This ensures that white (#ffffff) can be specified with the short notation (#fff) and removes any dependencies on the color depth of the display.  @see http://www.w3.org/TR/css3-color/
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@4763 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/test/java/com/itextpdf/text/html/WebColorTest.java
+++ itext/src/test/java/com/itextpdf/text/html/WebColorTest.java
@@ -44,6 +44,8 @@
 package com.itextpdf.text.html;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import com.itextpdf.text.BaseColor;
 
@@ -58,15 +60,22 @@ public class WebColorTest {
 	@Test
 	public void goodColorTests() throws Exception {
 		String colors[] = {
-				"#808080", "808080", "#888", "888", "gray", "rgb(128,128,128)"
+				"#00FF00", "00FF00", "#0F0", "0F0", "lime", "rgb(0,255,0 )"
 		};
 		// TODO webColor creates colors with a zero alpha channel (save "transparent"), BaseColor's
 		// 3-param constructor creates them with a 0xFF alpha channel.  Which is right?!
-		BaseColor testCol = new BaseColor(0x80, 0x80, 0x80);
+		BaseColor testCol = new BaseColor(0, 255, 0);
 		for (String colStr : colors) {
 			BaseColor curCol = WebColors.getRGBColor(colStr);
 			assertTrue(dumpColor(testCol) + "!=" + dumpColor(curCol), testCol.equals(curCol));
 		}
+	}
+
+	@Test
+	public void moreColorTest() {
+	    String colorStr = "#888";
+	    String colorStrLong = "#888888";
+	    assertEquals("Oh Nooo colors are different", WebColors.getRGBColor(colorStr), WebColors.getRGBColor(colorStrLong));
 	}
 
 	private String dumpColor(BaseColor col) {
@@ -79,21 +88,21 @@ public class WebColorTest {
 		colBuf.append(col.getBlue());
 		colBuf.append(" a:");
 		colBuf.append(col.getAlpha());
-		
+
 		return colBuf.toString();
 	}
-	
+
 	@Test
 	public void badColorTests() throws Exception {
 		String badColors[] = {
 				"", null, "#xyz", "#12345", "notAColor"
 		};
-		
+
 		for (String curStr : badColors) {
 			try {
 				// we can ignore the return value that'll never happen here
 				WebColors.getRGBColor(curStr);
-				
+
 				assertTrue("getRGBColor should have thrown for: " + curStr, false);
 			} catch (IllegalArgumentException e) {
 				// Non-null bad colors will throw an illArgEx
@@ -105,6 +114,6 @@ public class WebColorTest {
 			}
 		}
 	}
-	
-	
+
+
 }
