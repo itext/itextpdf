0f4d7faa7f18ed2bbe1dc14fc5fd0e738d7b7715
Paulo Soares <pgpsoares@gmail.com>
2011-03-28 20:40:57 +0000

    Opening an AES-256 encrypted PDF with a null password would throw a null exception. It will now behave correctly and throw a BadPasswordException if the password is wrong.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@4789 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/main/java/com/itextpdf/text/pdf/PdfEncryption.java
+++ itext/src/main/java/com/itextpdf/text/pdf/PdfEncryption.java
@@ -53,6 +53,7 @@ import java.security.MessageDigest;
 import java.security.cert.Certificate;
 
 import com.itextpdf.text.ExceptionConverter;
+import com.itextpdf.text.exceptions.BadPasswordException;
 import com.itextpdf.text.pdf.crypto.AESCipherCBCnoPad;
 import com.itextpdf.text.pdf.crypto.IVGenerator;
 import java.security.DigestException;
@@ -411,8 +412,10 @@ public class PdfEncryption {
     private static final int SALT_LENGHT = 8;
     private static final int OU_LENGHT = 48;
 
-    public boolean readKey(PdfDictionary enc, byte[] password) {
+    public boolean readKey(PdfDictionary enc, byte[] password) throws BadPasswordException {
         try {
+            if (password == null)
+                password = new byte[0];
             byte[] oValue = com.itextpdf.text.DocWriter.getISOBytes(enc.get(PdfName.O).toString());
             byte[] uValue = com.itextpdf.text.DocWriter.getISOBytes(enc.get(PdfName.U).toString());
             byte[] oeValue = com.itextpdf.text.DocWriter.getISOBytes(enc.get(PdfName.OE).toString());
@@ -439,7 +442,7 @@ public class PdfEncryption {
                 hash = md.digest();
                 isUserPass = compareArray(hash, uValue, 32);
                 if (!isUserPass)
-                    throw new IllegalArgumentException(MessageLocalization.getComposedMessage("bad.user.password"));
+                    throw new BadPasswordException(MessageLocalization.getComposedMessage("bad.user.password"));
                 md.update(password, 0, Math.min(password.length, 127));
                 md.update(uValue, KEY_SALT_OFFSET, SALT_LENGHT);
                 hash = md.digest();
@@ -449,11 +452,14 @@ public class PdfEncryption {
             AESCipherCBCnoPad ac = new AESCipherCBCnoPad(false, key);
             byte[] decPerms = ac.processBlock(perms, 0, perms.length);
             if (decPerms[9] != (byte)'a' || decPerms[10] != (byte)'d' || decPerms[11] != (byte)'b')
-                throw new IllegalArgumentException(MessageLocalization.getComposedMessage("bad.user.password"));
+                throw new BadPasswordException(MessageLocalization.getComposedMessage("bad.user.password"));
             permissions = (decPerms[0] & 0xff) | ((decPerms[1] & 0xff) << 8)
                     | ((decPerms[2] & 0xff) << 16) | ((decPerms[2] & 0xff) << 24);
             encryptMetadata = decPerms[8] == (byte)'T';
             return isOwnerPass;
+        }
+        catch (BadPasswordException ex) {
+            throw ex;
         }
         catch (Exception ex) {
             throw new ExceptionConverter(ex);
