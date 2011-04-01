f94fcb63e0c69eafed4fe35dc717cf827746f3b3
Paulo Soares <pgpsoares@gmail.com>
2011-04-01 20:49:40 +0000

    Include all the certificates in the signature (Vroonen Julien).
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@4790 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/main/java/com/itextpdf/text/pdf/PdfSigGenericPKCS.java
+++ itext/src/main/java/com/itextpdf/text/pdf/PdfSigGenericPKCS.java
@@ -95,12 +95,21 @@ public abstract class PdfSigGenericPKCS extends PdfSignature {
             pkcs = new PdfPKCS7(privKey, certChain, crlList, hashAlgorithm, provider, PdfName.ADBE_PKCS7_SHA1.equals(get(PdfName.SUBFILTER)));
             pkcs.setExternalDigest(externalDigest, externalRSAdata, digestEncryptionAlgorithm);
             if (PdfName.ADBE_X509_RSA_SHA1.equals(get(PdfName.SUBFILTER))) {
-                ByteArrayOutputStream bout = new ByteArrayOutputStream();
-                for (int k = 0; k < certChain.length; ++k) {
-                    bout.write(certChain[k].getEncoded());
+                if (certChain.length > 1) {
+                    PdfArray arr = new PdfArray();
+                    for (int ii = 0; ii < certChain.length; ii++) {
+                        arr.add(new PdfString(certChain[ii].getEncoded()));
+                    }
+                    put(PdfName.CERT, arr);
                 }
-                bout.close();
-                setCert(bout.toByteArray());
+                else {
+                    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+                    for (int k = 0; k < certChain.length; ++k) {
+                        bout.write(certChain[k].getEncoded());
+                    }
+                    bout.close();
+                    setCert(bout.toByteArray());
+                }
                 setContents(pkcs.getEncodedPKCS1());
             }
             else
