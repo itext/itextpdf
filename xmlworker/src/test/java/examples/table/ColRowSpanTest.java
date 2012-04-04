2558fe09151cbdd863e6a03b77d5287414d68f1b
Eugene Markovskyi <eugene.markovskyi@duallab.com>
2012-04-04 11:31:55 +0000

    Test was ignored bu Maven
    git-svn-id: https://svn.code.sf.net/p/xmlworker/code-0/trunk@309 7cc6213b-955e-4e79-b9b9-6c4fb085f9d1

--- xmlworker/src/test/java/examples/table/ColRowSpanTest.java
+++ xmlworker/src/test/java/examples/table/ColRowSpanTest.java
@@ -2,7 +2,7 @@ package examples.table;
 
 import examples.SampleTest;
 
-public class ColRowSpan extends SampleTest{
+public class ColRowSpanTest extends SampleTest{
     protected String getTestName() {
         return  "3_8-BF-06";
     }
