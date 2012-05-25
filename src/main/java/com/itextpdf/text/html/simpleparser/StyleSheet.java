62b3ae3a978cd799a081be27487592d08743806a
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-05-25 08:04:17 +0000

    Deprecating HTMLWorker and StyleSheet in favor of XML Worker.

--- src/main/java/com/itextpdf/text/html/simpleparser/StyleSheet.java
+++ src/main/java/com/itextpdf/text/html/simpleparser/StyleSheet.java
@@ -51,6 +51,11 @@ import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.html.HtmlTags;
 import com.itextpdf.text.html.HtmlUtilities;
 
+/**
+ * Old class to define styles for HTMLWorker.
+ * We've completely rewritten HTML to PDF functionality; see project XML Worker.
+ * XML Worker is able to parse CSS files and "style" attribute values.
+ */
 public class StyleSheet {
 
 	/**
