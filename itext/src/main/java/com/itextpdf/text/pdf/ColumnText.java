fbc540992ab6c6711746e40e13b8f7cee32345c7
Eugene Markovskyi <eugene.markovskyi@duallab.com>
2012-06-15 10:50:23 +0000

    Small XFA ColumnText improvement. If compositeColumn(inside goComposite(...)) does note place any text lines, the descent should not be updated.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@5182 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/main/java/com/itextpdf/text/pdf/ColumnText.java
+++ itext/src/main/java/com/itextpdf/text/pdf/ColumnText.java
@@ -1319,9 +1319,11 @@ public class ColumnText {
                     }
                 }
                 firstPass = false;
-                yLine = compositeColumn.yLine;
-                linesWritten += compositeColumn.linesWritten;
-                descender = compositeColumn.descender;
+                if (compositeColumn.getLinesWritten() > 0) {
+                    yLine = compositeColumn.yLine;
+                    linesWritten += compositeColumn.linesWritten;
+                    descender = compositeColumn.descender;
+                }
                 if ((status & NO_MORE_TEXT) != 0) {
                     compositeColumn = null;
                     compositeElements.removeFirst();
