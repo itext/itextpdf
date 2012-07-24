7f1600a3e3260a90fdeef0752133cef246ad8d7c
Eugene Markovskyi <eugene.markovskyi@duallab.com>
2012-07-24 19:36:05 +0000

    Introduction of a LINEHEIGHT attribute in class Chunk.
    git-svn-id: https://svn.code.sf.net/p/xmlworker/code-0/trunk@352 7cc6213b-955e-4e79-b9b9-6c4fb085f9d1

---
+++ xmlworker/src/test/java/com/itextpdf/tool/xml/examples/css/line_height/span/LineHeightSpan001Test.java
@@ -0,0 +1,10 @@
+package com.itextpdf.tool.xml.examples.css.line_height.span;
+
+import com.itextpdf.tool.xml.examples.SampleTest;
+
+public class LineHeightSpan001Test extends SampleTest {
+    protected String getTestName() {
+        return "lineHeightSpan001";
+    }
+
+}
