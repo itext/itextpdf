ad34ff82ab345b3e00f148854f9bc312867936f1
Eugene Markovskyi <eugene.markovskyi@duallab.com>
2012-07-26 08:19:48 +0000

    ColumnText leading logic improving. currentLeading are computed based on Chunk font size and leading.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@5247 3a709846-db2a-4fbd-be0b-eb932eee6479

---
+++ xmlworker/src/test/java/com/itextpdf/tool/xml/examples/css/line_height/span/LineHeightSpan003Test.java
@@ -0,0 +1,9 @@
+package com.itextpdf.tool.xml.examples.css.line_height.span;
+
+import com.itextpdf.tool.xml.examples.SampleTest;
+
+public class LineHeightSpan003Test extends SampleTest {
+    protected String getTestName() {
+        return "lineHeightSpan003";
+    }
+}
