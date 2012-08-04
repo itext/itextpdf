1a7d01fd92b131aa693f8bcecb8354c0c7b3fa03
Paulo Soares <pgpsoares@gmail.com>
2012-08-04 14:17:57 +0000

    Tiff colormaps are 16 bit but some tiff producers just put 8 bit resulting in black images. For these broken tiffs we try again using only 8 bits.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@5268 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/main/java/com/itextpdf/text/pdf/codec/TiffImage.java
+++ itext/src/main/java/com/itextpdf/text/pdf/codec/TiffImage.java
@@ -504,6 +504,23 @@ public class TiffImage {
                         palette[k * 3 + 1] = (byte)(rgb[k + gColor] >>> 8);
                         palette[k * 3 + 2] = (byte)(rgb[k + bColor] >>> 8);
                     }
+                    // Colormap components are supposed to go from 0 to 655535 but,
+                    // as usually, some tiff producers just put values from 0 to 255.
+                    // Let's check for these broken tiffs.
+                    boolean colormapBroken = true;
+                    for (int k = 0; k < palette.length; ++k) {
+                        if (palette[k] != 0) {
+                            colormapBroken = false;
+                            break;
+                        }
+                    }
+                    if (colormapBroken) {
+                        for (int k = 0; k < gColor; ++k) {
+                            palette[k * 3] = (byte)rgb[k];
+                            palette[k * 3 + 1] = (byte)rgb[k + gColor];
+                            palette[k * 3 + 2] = (byte)rgb[k + bColor];
+                        }
+                    }
                     PdfArray indexed = new PdfArray();
                     indexed.add(PdfName.INDEXED);
                     indexed.add(PdfName.DEVICERGB);
