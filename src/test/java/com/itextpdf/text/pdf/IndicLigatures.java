e053c28c26913e911e67254aa68872bf54bd93d5
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-11-26 10:19:15 +0000

    Added an isRTL() method to the LanguageProcessor interface as well as to its implementations. Made a small change that allows the correct rendering of text when the Virima character is missing.

--- src/test/java/com/itextpdf/text/pdf/IndicLigatures.java
+++ src/test/java/com/itextpdf/text/pdf/IndicLigatures.java
@@ -18,9 +18,23 @@ public class IndicLigatures {
     }
     
     @Test
+    public void testDevanagari2() throws Exception {
+    	IndicLigaturizer d = new DevanagariLigaturizer();
+    	String processed = d.process("\u0936\u093e\u0902\u0924\u093f");
+    	Assert.assertEquals("\u0936\u093e\u0902\u093f\u0924", processed);
+    }
+
+    @Test
     public void testGujarati() throws Exception {
     	IndicLigaturizer g = new GujaratiLigaturizer();
     	String processed = g.process("\u0ab6\u0abe\u0a82\u0aa4\u0acd\u0abf");
+    	Assert.assertEquals("\u0ab6\u0abe\u0a82\u0abf\u0aa4", processed);
+    }
+    
+    @Test
+    public void testGujarati2() throws Exception {
+    	IndicLigaturizer g = new GujaratiLigaturizer();
+    	String processed = g.process("\u0ab6\u0abe\u0a82\u0aa4\u0abf");
     	Assert.assertEquals("\u0ab6\u0abe\u0a82\u0abf\u0aa4", processed);
     }
 }
