688079a8aeb5acdcb7f26e2d011f73efee4378e2
Kevin Day <kevin@trumpetinc.com>
2012-11-28 22:58:12 +0000

    Add MRU paging for huge files

--- src/main/java/com/itextpdf/text/io/FileChannelRandomAccessSource.java
+++ src/main/java/com/itextpdf/text/io/FileChannelRandomAccessSource.java
@@ -43,6 +43,8 @@ package com.itextpdf.text.io;
 
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 /**
  * A RandomAccessSource that is based on an underlying {@link FileChannel}.  The channel will be mapped into memory for efficient reads.
@@ -51,10 +53,13 @@ import java.nio.channels.FileChannel;
  * @since 5.3.5
  */
 class FileChannelRandomAccessSource extends GroupedRandomAccessSource implements RandomAccessSource {
-    private static final int DEFAULT_BUFSIZE = 1 << 28; 
+	// these values were selected based on parametric testing with extracting text content from a 2.3GB file.  These settings resulted in the best improvement over
+	// the single size MRU case (24% speed improvement)
+    private static final int DEFAULT_TOTAL_BUFSIZE = 1 << 26; 
+    private static final int DEFAULT_MAX_OPEN_BUFFERS = 16;
 
     /**
-     * The size of the buffers to use when mapping files into memory.  This must be greater than 0 and less than {@link Integer#MAX_VALUE}
+     * The size of each of the buffers to use when mapping files into memory.  This must be greater than 0 and less than {@link Integer#MAX_VALUE}
      */
     private final int bufferSize;
     
@@ -62,14 +67,21 @@ class FileChannelRandomAccessSource extends GroupedRandomAccessSource implements
      * The channel this source is based on
      */
     private final FileChannel channel;
+    
+    /**
+     * Most recently used list used to hold a number of mapped pages open at a time
+     */
+    private final MRU<RandomAccessSource> mru;
 
     /**
-     * Constructs a new {@link FileChannelRandomAccessSource} based on the specified FileChannel, with a default buffer size
+     * Constructs a new {@link FileChannelRandomAccessSource} based on the specified FileChannel, with a default buffer configuration.
+     * The default buffer configuration is currently 2^26 total paged bytes, spread across a maximum of 16 active buffers. This arrangement
+     * resulted in a 24% speed improvement over the single buffer case in parametric tests extracting text from a 2.3 GB file.
      * @param channel the channel to use as the backing store
      * @throws IOException if the channel cannot be opened or mapped
      */
     public FileChannelRandomAccessSource(FileChannel channel) throws IOException {
-		this(channel, DEFAULT_BUFSIZE);
+		this(channel, DEFAULT_TOTAL_BUFSIZE, DEFAULT_MAX_OPEN_BUFFERS);
 	}
     
     /**
@@ -78,10 +90,11 @@ class FileChannelRandomAccessSource extends GroupedRandomAccessSource implements
      * @param bufferSize the size of the buffers to use
      * @throws IOException if the channel cannot be opened or mapped
      */
-	public FileChannelRandomAccessSource(final FileChannel channel, final int bufferSize) throws IOException {
-        super(buildSources(channel, bufferSize));
+	public FileChannelRandomAccessSource(final FileChannel channel, final int totalBufferSize, final int maxOpenBuffers) throws IOException {
+        super(buildSources(channel, totalBufferSize/maxOpenBuffers));
 		this.channel = channel;
-        this.bufferSize = bufferSize;
+        this.bufferSize = totalBufferSize/maxOpenBuffers;
+        this.mru = new MRU<RandomAccessSource>(maxOpenBuffers);
 	}
 
 	/**
@@ -119,7 +132,9 @@ class FileChannelRandomAccessSource extends GroupedRandomAccessSource implements
 	 * For now, close the source that is no longer being used.  In the future, we may implement an MRU that allows multiple pages to be opened at a time
 	 */
 	protected void sourceReleased(RandomAccessSource source) throws IOException {
-		source.close();
+		RandomAccessSource old = mru.enqueue(source);
+		if (old != null)
+			old.close();
 	}
 	
 	@Override
@@ -141,4 +156,49 @@ class FileChannelRandomAccessSource extends GroupedRandomAccessSource implements
         channel.close();
     }
 
+	private static class MRU<E>{
+		/**
+		 * The maximum number of entries held by this MRU
+		 */
+		private final int limit;
+		
+		/**
+		 * Backing list for managing the MRU
+		 */
+		private LinkedList<E> queue = new LinkedList<E>();
+
+		/**
+		 * Constructs an MRU with the specified size
+		 * @param limit the limit
+		 */
+		public MRU(int limit) {
+			this.limit = limit;
+		}
+		
+		/**
+		 * Adds an element to the MRU.  If the element is already in the MRU, it is moved to the top.
+		 * @param newElement the element to add
+		 * @return the element that was removed from the MRU to make room for the new element, or null if no element needed to be removed
+		 */
+		public E enqueue(E newElement){
+			// TODO: this check may not be an effective optimization - the GroupedRandomAccessSource already tracks the 'current' source, so it seems unlikely that we would ever hit this code branch
+			if (queue.size() > 0 && queue.getFirst() == newElement)
+				return null;
+			
+			for(Iterator<E> it = queue.iterator(); it.hasNext();){
+				E element = it.next();
+				if (newElement == element){
+					it.remove();
+					queue.addFirst(newElement);
+					return null;
+				}
+			}
+			queue.addFirst(newElement);
+			
+			if (queue.size() > limit)
+				return queue.removeLast();
+			
+			return null;
+		}
+	}
 }
