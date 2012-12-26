f0fe27bcae8d36ec728dbdc248b77b761cc6d09d
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-12-26 12:26:48 +0000

    Use of a constructor that was introduced in Java 6

--- src/main/java/com/itextpdf/text/io/MapFailedException.java
+++ src/main/java/com/itextpdf/text/io/MapFailedException.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 @SuppressWarnings("serial")
 public class MapFailedException extends IOException {
 	public MapFailedException(IOException e) {
-		super(e);
+        super(e.getMessage());
+        initCause(e);
 	}
 }
