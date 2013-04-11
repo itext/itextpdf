99d9c11c1c49690ec74aa00323913ed1b3985008
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-04-11 12:31:39 +0000

    5.4.1-SNAPSHOT -> 5.4.2-SNAPSHOT
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@5752 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/main/java/com/itextpdf/text/Version.java
+++ itext/src/main/java/com/itextpdf/text/Version.java
@@ -66,7 +66,7 @@ public final class Version {
 	 * This String contains the version number of this iText release.
 	 * For debugging purposes, we request you NOT to change this constant.
 	 */
-	private String release = "5.4.1-SNAPSHOT";
+	private String release = "5.4.2-SNAPSHOT";
 	
 	/**
 	 * The license key.
