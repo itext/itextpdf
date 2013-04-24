f2cacd53a17018381f225c52a3db6a4b38eca715
Alexander Chingarev <alexander.chingarev@duallab.com>
2013-04-24 15:05:49 +0000

    Added Jpeg2000 PDF/A conformance checks
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@5767 3a709846-db2a-4fbd-be0b-eb932eee6479

--- pdfa/src/main/java/com/itextpdf/text/pdf/internal/PdfA2Checker.java
+++ pdfa/src/main/java/com/itextpdf/text/pdf/internal/PdfA2Checker.java
@@ -1,14 +1,16 @@
 package com.itextpdf.text.pdf.internal;
 
+import com.itextpdf.text.Jpeg2000;
 import com.itextpdf.text.error_messages.MessageLocalization;
 import com.itextpdf.text.pdf.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
 public class PdfA2Checker extends PdfA1Checker {
 
-    static private HashSet<PdfName> allowedBlendModes = new HashSet<PdfName>(Arrays.asList(new PdfName[] {PdfGState.BM_NORMAL, PdfGState.BM_COMPATIBLE,
+    static private HashSet<PdfName> allowedBlendModes = new HashSet<PdfName>(Arrays.asList(new PdfName[]{PdfGState.BM_NORMAL, PdfGState.BM_COMPATIBLE,
             PdfGState.BM_MULTIPLY, PdfGState.BM_SCREEN, PdfGState.BM_OVERLAY, PdfGState.BM_DARKEN, PdfGState.BM_LIGHTEN, PdfGState.BM_COLORDODGE,
             PdfGState.BM_COLORBURN, PdfGState.BM_HARDLIGHT, PdfGState.BM_SOFTLIGHT, PdfGState.BM_DIFFERENCE, PdfGState.BM_EXCLUSION}));
 
@@ -20,5 +22,52 @@ public class PdfA2Checker extends PdfA1Checker {
             throw new PdfAConformanceException(MessageLocalization.getComposedMessage("blend.mode.1.not.allowed", obj.toString()));
         }
     }
+
+    @Override
+    protected void checkImage(PdfWriter writer, int key, Object obj1) {
+        PdfImage pdfImage = (PdfImage) obj1;
+        if (pdfImage != null && (pdfImage.getImage() instanceof Jpeg2000)) {
+            Jpeg2000 jpeg2000 = (Jpeg2000) pdfImage.getImage();
+            if (!jpeg2000.isJp2()) {
+                throw new PdfAConformanceException(MessageLocalization.getComposedMessage("only.jpx.baseline.set.of.features.shall.be.used"));
+            }
+            if (jpeg2000.getNumOfComps() != 1 && jpeg2000.getNumOfComps() != 3 && jpeg2000.getNumOfComps() != 4) {
+                throw new PdfAConformanceException(MessageLocalization.getComposedMessage("the.number.of.colour.channels.in.the.jpeg2000.data.shall.be.123"));
+            }
+            if (jpeg2000.getBpc() < 1 || jpeg2000.getBpc() > 38) {
+                throw new PdfAConformanceException(MessageLocalization.getComposedMessage("the.bit-depth.of.the.jpeg2000.data.shall.have.a.value.in.the.range.1to38"));
+            }
+            if (jpeg2000.getBpcBoxData() != null) {
+                throw new PdfAConformanceException(MessageLocalization.getComposedMessage("all.colour.channels.in.the.jpeg2000.data.shall.have.the.same.bit-depth"));
+            }
+            ArrayList<Jpeg2000.ColorSpecBox> colorSpecBoxes = jpeg2000.getColorSpecBoxes();
+            if (colorSpecBoxes != null) {
+                if (colorSpecBoxes.size() > 1) {
+                    int approx0x01 = 0;
+                    for (Jpeg2000.ColorSpecBox colorSpecBox : colorSpecBoxes) {
+                        if (colorSpecBox.getApprox() == 1)
+                            approx0x01++;
+                    }
+                    if (approx0x01 != 1) {
+                        throw new PdfAConformanceException(MessageLocalization.getComposedMessage("exactly.one.colour.space.specification.shall.have.the.value.0x01.in.the.approx.field"));
+                    }
+                }
+                for (Jpeg2000.ColorSpecBox colorSpecBox : colorSpecBoxes) {
+                    if (colorSpecBox.getMeth() != 1 && colorSpecBox.getMeth() != 2 && colorSpecBox.getMeth() != 3) {
+                        throw new PdfAConformanceException(MessageLocalization.getComposedMessage("the.value.of.the.meth.entry.in.colr.box.shall.be.123"));
+                    }
+                    if (colorSpecBox.getEnumCs() == 19) {
+                        throw new PdfAConformanceException(MessageLocalization.getComposedMessage("jpeg2000.enumerated.colour.space.19.(CIEJab).shall.not.be.used"));
+                    }
+                    byte[] colorProfileBytes = colorSpecBox.getColorProfile();
+                    if (colorProfileBytes != null) {
+                        //ICC profile verification should follow here.
+                    }
+                }
+
+            }
+        }
+    }
+
 
 }
--- pdfa/src/test/java/com/itextpdf/text/pdf/PdfA2CheckerTest.java
+++ pdfa/src/test/java/com/itextpdf/text/pdf/PdfA2CheckerTest.java
@@ -2,12 +2,14 @@ package com.itextpdf.text.pdf;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
 import junit.framework.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PdfA2CheckerTest {
 
@@ -49,13 +51,95 @@ public class PdfA2CheckerTest {
         } catch (PdfAConformanceException pdface) {
             exception = true;
         }
-        if (!exception)
-            Assert.fail("PdfAConformance exception should be thrown on unknown blend mode.");
 
         if (!exception)
             document.close();
+        tmpPdf.delete();
 
+        if (!exception)
+            Assert.fail("PdfAConformance exception should be thrown on unknown blend mode.");
+
+    }
+
+    @Test
+    public void imageCheckTest1() throws IOException, DocumentException {
+        File tmpPdf = File.createTempFile("pdfa2_", ".pdf");
+        FileOutputStream fos = new FileOutputStream(tmpPdf);
+        Document document = new Document();
+        PdfWriter writer = PdfAWriter.getInstance(document, fos, PdfAConformanceLevel.PDF_A_2A);
+        document.open();
+
+        String[] pdfaErrors = new String[9];
+        for (int i = 1; i <= 9; i++) {
+            try {
+                Image img = Image.getInstance(String.format("./src/test/resources/com/itextpdf/text/pdf/jpeg2000/file%s.jp2", Integer.toString(i)));
+                document.add(img);
+                document.newPage();
+            } catch (Exception e) {
+                pdfaErrors[i - 1] = e.getLocalizedMessage();
+            }
+        }
+
+        Assert.assertEquals(null, pdfaErrors[0]);
+        Assert.assertEquals(null, pdfaErrors[1]);
+        Assert.assertEquals(null, pdfaErrors[2]);
+        Assert.assertEquals(null, pdfaErrors[3]);
+        Assert.assertEquals(true, pdfaErrors[4].contains("0x01"));
+        Assert.assertEquals(null, pdfaErrors[5]);
+        Assert.assertEquals(true, pdfaErrors[6].contains("0x01"));
+        Assert.assertEquals(null, pdfaErrors[7]);
+        Assert.assertEquals(null, pdfaErrors[8]);
+
+        document.close();
         tmpPdf.delete();
     }
+
+    @Test
+    public void imageCheckTest2() throws IOException, DocumentException {
+        File tmpPdf = File.createTempFile("pdfa2_", ".pdf");
+        FileOutputStream fos = new FileOutputStream(tmpPdf);
+        Document document = new Document();
+        PdfWriter writer = PdfAWriter.getInstance(document, fos, PdfAConformanceLevel.PDF_A_2A);
+        document.open();
+
+        ArrayList<String> pdfaErrors = new ArrayList<String>();
+        try {
+            Image img = Image.getInstance("./src/test/resources/com/itextpdf/text/pdf/jpeg2000/p0_01.j2k");
+            document.add(img);
+            document.newPage();
+        } catch (Exception e) {
+            pdfaErrors.add(e.getLocalizedMessage());
+        }
+
+        try {
+            Image img = Image.getInstance("./src/test/resources/com/itextpdf/text/pdf/jpeg2000/p0_02.j2k");
+            document.add(img);
+        } catch (Exception e) {
+            pdfaErrors.add(e.getLocalizedMessage());
+        }
+
+        try {
+            Image img = Image.getInstance("./src/test/resources/com/itextpdf/text/pdf/jpeg2000/p1_01.j2k");
+            document.add(img);
+        } catch (Exception e) {
+            pdfaErrors.add(e.getLocalizedMessage());
+        }
+
+        try {
+            Image img = Image.getInstance("./src/test/resources/com/itextpdf/text/pdf/jpeg2000/p1_02.j2k");
+            document.add(img);
+        } catch (Exception e) {
+            pdfaErrors.add(e.getLocalizedMessage());
+        }
+
+        Assert.assertEquals(4, pdfaErrors.size());
+        for (int i = 0; i < 4; i++) {
+            Assert.assertEquals(true, pdfaErrors.get(i).contains("JPX"));
+        }
+
+        document.close();
+        tmpPdf.delete();
+    }
+
 
 }
