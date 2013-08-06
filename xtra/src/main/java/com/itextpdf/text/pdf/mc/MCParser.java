f3d59ca6cf0d19251e2740a57d84a31296558f17
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-08-06 14:53:18 +0000

    Experimental code to flatten forms in a tagged PDF.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@5931 3a709846-db2a-4fbd-be0b-eb932eee6479

---
+++ xtra/src/main/java/com/itextpdf/text/pdf/mc/MCParser.java
@@ -0,0 +1,364 @@
+/*
+ * $Id$
+ *
+ * This file is part of the iText (R) project.
+ * Copyright (c) 1998-2013 1T3XT BVBA
+ * Authors: Bruno Lowagie, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY 1T3XT,
+ * 1T3XT DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA, 02110-1301 USA, or download the license from the following URL:
+ * http://itextpdf.com/terms-of-use/
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every PDF that is created
+ * or manipulated using iText.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the iText software without
+ * disclosing the source code of your own applications.
+ * These activities include: offering paid services to customers as an ASP,
+ * serving PDFs on the fly in a web application, shipping iText with a closed
+ * source product.
+ *
+ * For more information, please contact iText Software Corp. at this
+ * address: sales@itextpdf.com
+ */
+package com.itextpdf.text.pdf.mc;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.io.RandomAccessSourceFactory;
+import com.itextpdf.text.pdf.PRStream;
+import com.itextpdf.text.pdf.PRTokeniser;
+import com.itextpdf.text.pdf.PdfArray;
+import com.itextpdf.text.pdf.PdfContentParser;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfIndirectReference;
+import com.itextpdf.text.pdf.PdfLiteral;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfNumber;
+import com.itextpdf.text.pdf.PdfObject;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStream;
+import com.itextpdf.text.pdf.RandomAccessFileOrArray;
+
+public class MCParser {
+
+	/** Factory that will help us build a RandomAccessSource. */
+	protected RandomAccessSourceFactory factory = new RandomAccessSourceFactory();
+    
+	/** A map with all supported operators operators (PDF syntax). */
+    protected static Map<String, PdfOperator> operators = null;
+	
+	/** Constant used for the default operator. */
+    public static final String DEFAULTOPERATOR = "DefaultOperator";
+    
+    /** The OutputStream of this worker object. */
+    protected static ByteArrayOutputStream baos;
+
+    /** The list with structure items. */
+    protected List<StructureItem> items;
+    
+    /** the XObject dictionary of the page that is being processed. */
+    protected PdfDictionary xobjects;
+    
+    /** Did we postpone writing a BT operator? */
+    protected boolean btWrite = false;
+    
+    /** Are we inside a BT/ET sequence? */
+    protected boolean inText = false;
+    
+    /**
+     * Creates an MCParser object.
+     * @param items a list of StructureItem objects
+     */
+    public MCParser(List<StructureItem> items) {
+    	populateOperators();
+    	this.items = items;
+    }
+    
+    /**
+     * Parses the content of a page, replacing appearances of annotations
+     * with Form XObjects.
+     * @param page a page dictionary
+     * @throws IOException
+     */
+    public void parse(PdfDictionary page) throws IOException {
+    	baos = new ByteArrayOutputStream();
+    	PdfDictionary resources = page.getAsDict(PdfName.RESOURCES);
+    	xobjects = resources.getAsDict(PdfName.XOBJECT);
+    	if (xobjects == null) {
+    		xobjects = new PdfDictionary();
+    		resources.put(PdfName.XOBJECT, xobjects);
+    	}
+		PRStream stream = (PRStream)page.getAsStream(PdfName.CONTENTS);
+		byte[] contentBytes = PdfReader.getStreamBytes(stream);
+        PRTokeniser tokeniser = new PRTokeniser(new RandomAccessFileOrArray(factory.createSource(contentBytes)));
+        PdfContentParser ps = new PdfContentParser(tokeniser);
+        ArrayList<PdfObject> operands = new ArrayList<PdfObject>();
+        while (ps.parse(operands).size() > 0){
+            PdfLiteral operator = (PdfLiteral)operands.get(operands.size() - 1);
+            processOperator(operator, operands);
+        }
+        baos.flush();
+        baos.close();
+        stream.setData(baos.toByteArray());
+    }
+    
+    /**
+     * Processes an operator, for instance: write the operator and its operands to baos.
+     * @param operator	the operator
+     * @param operands	the operator's operands
+     * @throws IOException
+     */
+    protected void processOperator(PdfLiteral operator, List<PdfObject> operands) throws IOException {
+        PdfOperator op = operators.get(operator.toString());
+        if (op == null)
+            op = operators.get(DEFAULTOPERATOR);
+        op.process(this, operator, operands);
+    }
+    
+    /**
+     * Informs the parser that we're inside or outside a text object.
+     * Also sets a parameter indicating that BT needs to be written.
+     * @param inText	true if we're inside.
+     */
+    protected void setInText(boolean inText) {
+    	if (inText)
+    		btWrite = true;
+    	this.inText = inText;
+    }
+    
+    /**
+     * When an MCID is encountered, the parser will check the list
+     * structure items and turn an annotation into an XObject if
+     * necessary.
+     * @param mcid	the MCID that was encountered in the content stream
+     * @throws IOException
+     */
+    protected void dealWithMcid(PdfNumber mcid) throws IOException {
+    	if (mcid == null)
+    		return;
+    	StructureItem item = items.get(0);
+    	switch (item.process(mcid.intValue())) {
+    	case 0 :
+    		items.remove(0);
+    		convertToXObject(item);
+    		dealWithMcid(mcid);
+    		return;
+    	case 1 :
+    		items.remove(0);
+    		return;
+    	case 2:
+    		return;
+    	}
+    }
+
+    /**
+     * Converts an annotation structure item to a Form XObject annotation.
+     * @param item the structure item
+     * @throws IOException
+     */
+    protected void convertToXObject(StructureItem item) throws IOException {
+    	PdfDictionary dict = item.getObj();
+    	if (dict == null || !dict.checkType(PdfName.ANNOT))
+    		return;
+    	PdfDictionary ap = dict.getAsDict(PdfName.AP);
+    	if (ap == null)
+    		return;
+    	PdfNumber structParent = dict.getAsNumber(PdfName.STRUCTPARENT);
+    	if (structParent == null)
+    		return;
+    	PdfStream stream = ap.getAsStream(PdfName.N);
+    	if (stream == null)
+    		return;
+    	stream.put(PdfName.STRUCTPARENT, structParent);
+    	PdfIndirectReference xobjr = ap.getAsIndirectObject(PdfName.N);
+    	if (xobjr == null)
+    		return;
+    	item.getObjr().put(PdfName.OBJ, xobjr);
+    	PdfName xobj = new PdfName("XObj" + structParent.intValue());
+    	xobjects.put(xobj, xobjr);
+    	PdfArray array = dict.getAsArray(PdfName.RECT);
+    	Rectangle rect = new Rectangle(
+    			array.getAsNumber(0).floatValue(), array.getAsNumber(1).floatValue(),
+    			array.getAsNumber(2).floatValue(), array.getAsNumber(3).floatValue());
+    	rect.normalize();
+    	if (inText && !btWrite)
+    		baos.write("ET\n".getBytes());
+    	baos.write("q 1 0 0 1 ".getBytes());
+    	baos.write(String.valueOf(rect.getLeft()).getBytes());
+    	baos.write(" ".getBytes());
+    	baos.write(String.valueOf(rect.getBottom()).getBytes());
+    	baos.write(" cm ".getBytes());
+    	xobj.toPdf(null, baos);
+    	baos.write(" Do Q\n".getBytes());
+    	if (inText)
+    		btWrite = true;
+    }
+    
+    /**
+     * Adds an operator and its operands (if any) to baos.
+     * @param operator	the operator
+     * @param operands	its operands
+     * @throws IOException
+     */
+    protected void printOperator(PdfLiteral operator, List<PdfObject> operands) throws IOException{
+    	operands.remove(operator);
+		for (PdfObject o : operands) {
+			printsp(o);
+		}
+		println(operator);
+    }
+    
+    /**
+     * Writes a PDF object to the OutputStream, followed by a space character.
+     * @param o a PdfObject
+     * @throws IOException
+     */
+    protected void printsp(PdfObject o) throws IOException {
+    	checkBT();
+    	o.toPdf(null, baos);
+    	baos.write(' ');
+    }
+
+    /**
+     * Writes a PDF object to the OutputStream, followed by a newline character.
+     * @param o a PdfObject
+     * @throws IOException
+     */
+    protected void println(PdfObject o) throws IOException {
+    	checkBT();
+    	o.toPdf(null, baos);
+    	baos.write('\n');
+    }
+
+    /**
+     * Checks if a BT operator is waiting to be added.
+     */
+    protected void checkBT() throws IOException {
+    	if (btWrite)
+    		baos.write("BT ".getBytes());
+    	btWrite = false;
+    }
+    
+    /**
+     * Populates the operators variable.
+     */
+    protected void populateOperators() {
+    	if (operators != null)
+    		return;
+    	operators = new HashMap<String, PdfOperator>();
+    	operators.put(DEFAULTOPERATOR, new CopyContentOperator());
+    	PdfOperator markedContent = new BeginMarkedContentDictionaryOperator();
+    	operators.put("BDC", markedContent);
+    	PdfOperator beginText = new BeginTextOperator();
+    	operators.put("BT", beginText);
+    	PdfOperator endText = new EndTextOperator();
+    	operators.put("ET", endText);
+    }
+    
+    /**
+     * PDF Operator interface.
+     */
+    public interface PdfOperator {
+    	/**
+    	 * Methods that processes an operator
+    	 * @param parser	the parser
+    	 * @param operator	the operator
+    	 * @param operands	its operands
+    	 * @throws IOException
+    	 */
+    	public void process(MCParser parser, PdfLiteral operator, List<PdfObject> operands) throws IOException;
+    }
+
+
+    /**
+     * Class that processes content by just printing the operator and its operands.
+     */
+    private static class CopyContentOperator implements PdfOperator{
+    	
+		/**
+		 * @see com.itextpdf.text.pdf.ocg.OCGParser.PdfOperator#process(com.itextpdf.text.pdf.ocg.OCGParser, com.itextpdf.text.pdf.PdfLiteral, java.util.List)
+		 */
+		public void process(MCParser parser,
+				PdfLiteral operator, List<PdfObject> operands) throws IOException {
+			parser.printOperator(operator, operands);
+		}
+    }
+
+    /**
+     * Class that knows how to process marked content operators.
+     */
+    private static class BeginMarkedContentDictionaryOperator implements PdfOperator {
+
+		/**
+		 * @see com.itextpdf.text.pdf.ocg.OCGParser.PdfOperator#process(com.itextpdf.text.pdf.ocg.OCGParser, com.itextpdf.text.pdf.PdfLiteral, java.util.List)
+		 */
+    	public void process(MCParser parser, PdfLiteral operator,
+    			List<PdfObject> operands) throws IOException {
+    		if ("BDC".equals(operator.toString())) {
+    			if (operands.get(1).isDictionary()) {
+    				PdfDictionary dict = (PdfDictionary)operands.get(1);
+    				parser.dealWithMcid(dict.getAsNumber(PdfName.MCID));
+    			}
+    		}
+			parser.printOperator(operator, operands);
+    	}
+    }
+    
+    /**
+     * Class that knows how to process the BT operator.
+     */
+    private static class BeginTextOperator implements PdfOperator {
+
+		/**
+		 * @see com.itextpdf.text.pdf.mc.MCParser.PdfOperator#process(com.itextpdf.text.pdf.mc.MCParser, com.itextpdf.text.pdf.PdfLiteral, java.util.List)
+		 */
+		public void process(MCParser parser, PdfLiteral operator,
+				List<PdfObject> operands) throws IOException {
+			parser.setInText(true);
+		}
+    	
+    }
+    
+    /**
+     * Class that knows how to the ET operators.
+     */
+    private static class EndTextOperator implements PdfOperator {
+
+		/**
+		 * @see com.itextpdf.text.pdf.mc.MCParser.PdfOperator#process(com.itextpdf.text.pdf.mc.MCParser, com.itextpdf.text.pdf.PdfLiteral, java.util.List)
+		 */
+		public void process(MCParser parser, PdfLiteral operator,
+				List<PdfObject> operands) throws IOException {
+			parser.setInText(false);
+			parser.printOperator(operator, operands);
+		}
+    	
+    }
+}
---
+++ xtra/src/main/java/com/itextpdf/text/pdf/mc/StructureItem.java
@@ -0,0 +1,167 @@
+/*
+ * $Id$
+ *
+ * This file is part of the iText (R) project.
+ * Copyright (c) 1998-2013 1T3XT BVBA
+ * Authors: Bruno Lowagie, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY 1T3XT,
+ * 1T3XT DISCLAIMS THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA, 02110-1301 USA, or download the license from the following URL:
+ * http://itextpdf.com/terms-of-use/
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every PDF that is created
+ * or manipulated using iText.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the iText software without
+ * disclosing the source code of your own applications.
+ * These activities include: offering paid services to customers as an ASP,
+ * serving PDFs on the fly in a web application, shipping iText with a closed
+ * source product.
+ *
+ * For more information, please contact iText Software Corp. at this
+ * address: sales@itextpdf.com
+ */
+package com.itextpdf.text.pdf.mc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.itextpdf.text.pdf.PdfArray;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfNumber;
+import com.itextpdf.text.pdf.PdfObject;
+
+/**
+ * Object that stores an item that is part of the document structure.
+ * It can refer to a Marked Content sequence in a page or an object
+ * reference.
+ */
+public class StructureItem {
+	
+	/** MarkedContent IDs in case we're dealing with an MC sequence on a page. */
+	List<Integer> mcids = new ArrayList<Integer>();
+	/** Object reference in case we're dealing with an object. */
+	PdfDictionary objr;
+	
+	/**
+	 * Creates a structure item based on a dictionary.
+	 * The dictionary can be of type StructElem, MCR or OBJR.
+	 * @param dict	the dictionary that needs to be examined
+	 */
+	public StructureItem(PdfDictionary dict) {
+		if (dict.checkType(PdfName.MCR)) {
+			mcids.add(dict.getAsNumber(PdfName.MCID).intValue());
+		}
+		else if (dict.checkType(PdfName.OBJR)) {
+			objr = dict;
+		}
+		else {
+			inspectKids(dict.getDirectObject(PdfName.K));
+		}
+	}
+	
+	/**
+	 * Inspects the value of a K entry and stores all MCIDs
+	 * that are encountered.
+	 * @param object the value of a K-entry
+	 */
+	protected void inspectKids(PdfObject object) {
+		if (object == null)
+			return;
+		switch(object.type()) {
+		case PdfObject.NUMBER:
+			mcids.add(((PdfNumber)object).intValue());
+			break;
+		case PdfObject.ARRAY:
+			PdfArray array = (PdfArray)object;
+			for (int i = 0; i < array.size(); i++) {
+				inspectKids(array.getDirectObject(i));
+			}
+		}
+	}
+	
+	/**
+	 * Checks if we're dealing with real content.
+	 * @return true if there's something to process
+	 */
+	public boolean isRealContent() {
+		return (mcids.size() > 0 || objr != null);
+	}
+
+	/**
+	 * Processes a MCID.
+	 * @param mcid the MCID
+	 * @return 0 in case there's an OBJR dictionary,
+	 *         1 in case all MCIDs are now encountered
+	 *         2 in case there are still MCIDs to process.
+	 */
+	public int process(int mcid) {
+		if (mcids.contains(mcid)) {
+			mcids.remove(new Integer(mcid));
+			return mcids.size() > 0 ? 2 : 1;
+		}
+		if (objr != null)
+			return 0;
+		return -1;
+	}
+
+	/**
+	 * Returns the object referred to by the OBJR dictionary.
+	 * Note that this method returns a dictionary which means
+	 * that only the stream dictionary will be passed in case
+	 * of an XObject.
+	 * @return the object referred to by OBJR as a dictionary
+	 */
+	public PdfDictionary getObj() {
+		if (objr == null)
+			return null;
+		return objr.getAsDict(PdfName.OBJ);
+	}
+	
+	/**
+	 * Returns the OBJR dictionary (if present).
+	 * @return a dictionary of type OBJR or null
+	 */
+	public PdfDictionary getObjr() {
+		return objr;
+	}
+	
+	/**
+	 * Creates a String representation of the object.
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		StringBuffer buf = new StringBuffer();
+		if (mcids.size() > 0) {
+			buf.append("MCID: ");
+			for (int i : mcids) {
+				buf.append(String.valueOf(i) + " ");
+			}
+		}
+		if (objr != null) {
+			buf.append(objr);
+		}
+		return buf.toString();
+	}
+}
