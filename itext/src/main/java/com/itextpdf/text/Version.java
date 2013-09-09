ae6fe9ce5cf25bcb68a21d76d47fbad3364cd95a
Alexander Chingarev <alexander.chingarev@duallab.com>
2013-09-09 11:47:29 +0000

    5.4.4-SNAPSHOT -> 5.4.5-SNAPSHOT
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@5999 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/main/java/com/itextpdf/text/Version.java
+++ itext/src/main/java/com/itextpdf/text/Version.java
@@ -66,7 +66,7 @@ public final class Version {
 	 * This String contains the version number of this iText release.
 	 * For debugging purposes, we request you NOT to change this constant.
 	 */
-	private String release = "5.4.4-SNAPSHOT";
+	private String release = "5.4.5-SNAPSHOT";
 	
 	/**
 	 * The license key.
