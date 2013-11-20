82e5e369edac1eaf7e85267037c97d4bf4bccaa7
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-11-20 13:54:29 +0000

    It doesn't make sense to set full compression after the document has been opened. This fixes a problem introduced by a change the ensured better handling of generation numbers.
    git-svn-id: https://svn.code.sf.net/p/itext/code/trunk@6076 3a709846-db2a-4fbd-be0b-eb932eee6479

--- itext/src/main/java/com/itextpdf/text/pdf/PdfCopyFields.java
+++ itext/src/main/java/com/itextpdf/text/pdf/PdfCopyFields.java
@@ -201,10 +201,8 @@ public class PdfCopyFields
     /**
      * Sets the document's compression to the new 1.5 mode with object streams and xref
      * streams. It can be set at any time but once set it can't be unset.
-     * <p>
-     * If set before opening the document it will also set the pdf version to 1.5.
      */
-    public void setFullCompression() {
+    public void setFullCompression() throws DocumentException {
         fc.setFullCompression();
     }
 
