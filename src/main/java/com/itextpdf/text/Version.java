2e490fa7671e3f667488cdd5ba3d11a142e8573c
Alexander Chingarev <alexander.chingarev@itextpdf.com>
2014-02-13 13:02:08 +0000

    version changed 5.4.6-SNAPSHOT -> 5.5.0

--- src/main/java/com/itextpdf/text/Version.java
+++ src/main/java/com/itextpdf/text/Version.java
@@ -67,7 +67,7 @@ public final class Version {
 	 * This String contains the version number of this iText release.
 	 * For debugging purposes, we request you NOT to change this constant.
 	 */
-	private String release = "5.4.6-SNAPSHOT";
+	private String release = "5.5.0";
 	
 	/**
 	 * The license key.
