74bc7e32a9da537265a29e25902372c18c13453b
Alexey Subach <alexey.subach@itextpdf.com>
2014-05-06 12:34:16 +0000

    DEV-911 apply the suggested fix in one more place.

--- src/main/java/com/itextpdf/text/pdf/FontSelector.java
+++ src/main/java/com/itextpdf/text/pdf/FontSelector.java
@@ -44,13 +44,13 @@
  */
 package com.itextpdf.text.pdf;
 
-import java.util.ArrayList;
-
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Utilities;
 import com.itextpdf.text.error_messages.MessageLocalization;
+
+import java.util.ArrayList;
 
 /** Selects the appropriate fonts that contain the glyphs needed to
  * render text correctly. The fonts are checked in order until the
@@ -133,7 +133,7 @@ public class FontSelector {
             } else {
                 for (int f = 0; f < fonts.size(); ++f) {
                     font = fonts.get(f);
-                    if (font.getBaseFont().charExists(c)) {
+                    if (font.getBaseFont().charExists(c) || Character.getType(c) == Character.FORMAT) {
                         if (currentFont != font) {
                             if (sb.length() > 0 && currentFont != null) {
                                 newChunk = new Chunk(sb.toString(), currentFont);
