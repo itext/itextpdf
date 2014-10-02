60ca0e28d16c0decb77faeb0fcde437bc3589328
Alexander Chingarev <alexander.chingarev@itextpdf.com>
2014-10-02 15:03:02 +0000

    PdfAChecker throws PDF/A-2 conformance exception when PdfAction chunk is added to the document. Fixed.

--- src/main/java/com/itextpdf/text/pdf/PdfWriter.java
+++ src/main/java/com/itextpdf/text/pdf/PdfWriter.java
@@ -3417,6 +3417,60 @@ public class PdfWriter extends DocWriter implements
         return new XmpWriter(baos, info);
     }
 
+    /**
+     * A wrapper around PdfAnnotation constructor.
+     * It is recommended to use this wrapper instead of direct constructor as this is a convenient way to override PdfAnnotation construction when needed.
+     *
+     * @param rect
+     * @param subtype
+     * @return
+     */
+    public PdfAnnotation createAnnotation(Rectangle rect, PdfName subtype) {
+        PdfAnnotation a = new PdfAnnotation(this, rect);
+        if (subtype != null)
+            a.put(PdfName.SUBTYPE, subtype);
+        return a;
+    }
+
+    /**
+     * A wrapper around PdfAnnotation constructor.
+     * It is recommended to use this wrapper instead of direct constructor as this is a convenient way to override PdfAnnotation construction when needed.
+     *
+     * @param llx
+     * @param lly
+     * @param urx
+     * @param ury
+     * @param title
+     * @param content
+     * @param subtype
+     * @return
+     */
+    public PdfAnnotation createAnnotation(float llx, float lly, float urx, float ury, PdfString title, PdfString content, PdfName subtype) {
+        PdfAnnotation a = new PdfAnnotation(this, llx, lly, urx, ury, title, content);
+        if (subtype != null)
+            a.put(PdfName.SUBTYPE, subtype);
+        return a;
+    }
+
+    /**
+     * A wrapper around PdfAnnotation constructor.
+     * It is recommended to use this wrapper instead of direct constructor as this is a convenient way to override PdfAnnotation construction when needed.
+     *
+     * @param llx
+     * @param lly
+     * @param urx
+     * @param ury
+     * @param action
+     * @param subtype
+     * @return
+     */
+    public PdfAnnotation createAnnotation(float llx, float lly, float urx, float ury, PdfAction action, PdfName subtype) {
+        PdfAnnotation a = new PdfAnnotation(this, llx, lly, urx, ury, action);
+        if (subtype != null)
+            a.put(PdfName.SUBTYPE, subtype);
+        return a;
+    }
+
     public static void checkPdfIsoConformance(PdfWriter writer, int key, Object obj1) {
         if (writer != null)
             writer.checkPdfIsoConformance(key, obj1);
