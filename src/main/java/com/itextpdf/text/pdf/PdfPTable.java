900e85fdd82a5b66d790331f47931c47ff65cc7c
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2015-01-23 15:05:58 +0000

    Reverting change: some tests were broken (Tagged PDF related).

--- src/main/java/com/itextpdf/text/pdf/PdfPTable.java
+++ src/main/java/com/itextpdf/text/pdf/PdfPTable.java
@@ -280,7 +280,7 @@ public class PdfPTable implements LargeElement, Spaceable, IAccessibleElement {
             PdfPRow row = table.rows.get(k);
             if (row != null) {
                 row = new PdfPRow(row);
-                row.calculated = false;
+                //row.calculated = false;
             }
             rows.add(row);
         }
