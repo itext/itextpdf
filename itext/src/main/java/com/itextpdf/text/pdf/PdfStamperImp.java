4fb89e58599da9f41bdb0bd79ba7b16769c7fb90
Pavel Morozov <pavel.morozov@duallab.com>
2015-05-29 12:13:11 +0300

    Added an opportunity to add named destinations in and existing file via PdfStamper (DEV-1420).

--- itext/src/main/java/com/itextpdf/text/pdf/PdfStamperImp.java
+++ itext/src/main/java/com/itextpdf/text/pdf/PdfStamperImp.java
@@ -101,6 +101,7 @@ class PdfStamperImp extends PdfWriter {
     protected IntHashtable marked;
     protected int initialXrefSize;
     protected PdfAction openAction;
+    protected HashMap<Object, PdfObject> namedDestinations = new HashMap<Object, PdfObject>();
 
     protected Counter COUNTER = CounterFactory.getCounter(PdfStamper.class);
     protected Counter getCounter() {
@@ -383,6 +384,9 @@ class PdfStamperImp extends PdfWriter {
         		markUsed(catalog);
         	}
         }
+
+        if (!namedDestinations.isEmpty())
+            updateNamedDestinations();
         close(info, skipInfo);
     }
 
@@ -1857,6 +1861,50 @@ class PdfStamperImp extends PdfWriter {
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
+    }
+
+    protected HashMap<Object, PdfObject> getNamedDestinations(){
+        return namedDestinations;
+    }
+
+    protected void updateNamedDestinations() throws IOException {
+
+        PdfDictionary dic = reader.getCatalog().getAsDict(PdfName.NAMES);
+        if (dic != null)
+            dic = dic.getAsDict(PdfName.DESTS);
+        if (dic == null){
+            dic = reader.getCatalog().getAsDict(PdfName.DESTS);
+        }
+        if (dic == null){
+            dic = new PdfDictionary();
+            PdfDictionary dests = new PdfDictionary();
+            dic.put(PdfName.NAMES, new PdfArray());
+            dests.put(PdfName.DESTS, dic);
+            reader.getCatalog().put(PdfName.NAMES, dests);
+        }
+
+        PdfArray names = getLastChildInNameTree(dic);
+
+        for (Object name : namedDestinations.keySet()){
+            names.add(new PdfString(name.toString()));
+            names.add(addToBody(namedDestinations.get(name), getPdfIndirectReference()).getIndirectReference());
+        }
+    }
+
+    private PdfArray getLastChildInNameTree(PdfDictionary dic){
+
+        PdfArray names;
+
+        PdfArray childNode = dic.getAsArray(PdfName.KIDS);
+        if (childNode != null){
+            PdfDictionary lastKid = childNode.getAsDict(childNode.size()-1);
+            names = getLastChildInNameTree(lastKid);
+        }
+        else {
+            names = dic.getAsArray(PdfName.NAMES);
+        }
+
+        return names;
     }
 
     static class PageStamp {
