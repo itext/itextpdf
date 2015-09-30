c771aac85db271e268e59a36c7a30c0f5c759b7a
Xiliang Chen <xlchen1291@gmail.com>
2015-10-01 11:39:00 +1300

    replace itext version
    Signed-off-by: Jordan Thoms <jordan@thoms.net.nz>

--- itext/src/main/java/com/itextpdf/text/Version.java
+++ itext/src/main/java/com/itextpdf/text/Version.java
@@ -69,19 +69,19 @@ public final class Version {
 	 * iText is a registered trademark by iText Group NV.
 	 * Please don't change this constant.
 	 */
-	private final String iText = "iText\u00ae";
+	private final String iText = "KamiHQ.com - document viewer and editor for the cloud";
 	/**
 	 * This String contains the version number of this iText release.
 	 * For debugging purposes, we request you NOT to change this constant.
 	 */
-	private final String release = "5.5.13.3";
+	private final String release = "1.0.1";
 	/**
 	 * This String contains the iText version as shown in the producer line.
 	 * iText is a product developed by iText Group NV.
 	 * iText Group requests that you retain the iText producer line
 	 * in every PDF that is created or manipulated using iText.
 	 */
-	private String iTextVersion = iText + " " + release + " \u00a92000-2022 iText Group NV";
+	private String iTextVersion = iText + " " + release + " ";
 	/**
 	 * The license key.
 	 */
@@ -93,58 +93,9 @@ public final class Version {
 	 * in every PDF that is created or manipulated using iText.
 	 */
 	public static Version getInstance() {
-	    synchronized (staticLock) {
-	        if (version != null) {
-	            return version;
-            }
-        }
-        Version localVersion = new Version();
-        try {
-            Class<?> klass = Class.forName("com.itextpdf.licensekey.LicenseKey");
-            Class[] cArg  = {String.class};
-            Method m = klass.getMethod("getLicenseeInfoForVersion",cArg);
-            Object[] args = {localVersion.release};
-            String[] info = (String[]) m.invoke(klass.newInstance(),args);
-            if (info[3] != null && info[3].trim().length() > 0) {
-                localVersion.key = info[3];
-            } else {
-                localVersion.key = "Trial version ";
-                if (info[5] == null) {
-                    localVersion.key += "unauthorised";
-                } else {
-                    localVersion.key += info[5];
-                }
-            }
-
-            if (info[4] != null && info[4].trim().length() > 0) {
-                localVersion.iTextVersion = info[4];
-            } else if (info[2] != null && info[2].trim().length() > 0) {
-                localVersion.iTextVersion += " (" + info[2];
-                if (!localVersion.key.toLowerCase().startsWith("trial")) {
-                    localVersion.iTextVersion += "; licensed version)";
-                } else {
-                    localVersion.iTextVersion += "; " + localVersion.key + ")";
-                }
-            } else if (info[0] != null && info[0].trim().length() > 0) {
-                // fall back to contact name, if company name is unavailable
-                localVersion.iTextVersion += " (" + info[0];
-                if (!localVersion.key.toLowerCase().startsWith("trial")) {
-                    // we shouldn't have a licensed version without company name,
-                    // but let's account for it anyway
-                    localVersion.iTextVersion += "; licensed version)";
-                } else {
-                    localVersion.iTextVersion += "; " + localVersion.key + ")";
-                }
-            } else {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            if (dependsOnTheOldLicense()) {
-                throw new RuntimeException("iText License Library 1.0.* has been deprecated. Please, update to the latest version.");
-            }
-            localVersion.iTextVersion += AGPL;
-        }
-		return atomicSetVersion(localVersion);
+		if (version == null) {
+            version = new Version();
+		return version;
 	}
 
 	private static boolean dependsOnTheOldLicense() {
