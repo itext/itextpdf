6cee6227d36ac7a48a4c17ba1bb13a34e9655e2d
Benoit Lagae <benoit.lagae@itextpdf.com>
2016-01-25 14:02:06 +0100

    Keep older API methods for TextChunk
    Some customers apparently use these methods, accessing them
    through reflection. While they should upgrade to take advantage of the new
    architecture, we will not force their hand.

--- itext/src/main/java/com/itextpdf/text/pdf/parser/LocationTextExtractionStrategy.java
+++ itext/src/main/java/com/itextpdf/text/pdf/parser/LocationTextExtractionStrategy.java
@@ -379,6 +379,10 @@ public class LocationTextExtractionStrategy implements TextExtractionStrategy {
         private final String text;
         private final TextChunkLocation location;
         
+        public TextChunk(String string, Vector startLocation, Vector endLocation, float charSpaceWidth) {
+            this(string, new TextChunkLocationDefaultImp(startLocation, endLocation, charSpaceWidth));
+        }
+        
         public TextChunk(String string, TextChunkLocation loc) {
             this.text = string;
             this.location = loc;
@@ -391,10 +395,44 @@ public class LocationTextExtractionStrategy implements TextExtractionStrategy {
         	return text;
         }
         
+        /**
+         * @return an object holding location data about this TextChunk
+         */
         public TextChunkLocation getLocation() {
             return location;
         }
         
+        /**
+         * @return the start location of the text
+         */
+        public Vector getStartLocation(){
+            return location.getStartLocation();
+        }
+        /**
+         * @return the end location of the text
+         */
+        public Vector getEndLocation(){
+            return location.getEndLocation();
+        }
+        
+        /**
+         * @return the width of a single space character as rendered by this chunk
+         */
+        public float getCharSpaceWidth() {
+            return location.getCharSpaceWidth();
+        }
+        
+        /**
+         * Computes the distance between the end of 'other' and the beginning of this chunk
+         * in the direction of this chunk's orientation vector.  Note that it's a bad idea
+         * to call this for chunks that aren't on the same line and orientation, but we don't
+         * explicitly check for that condition for performance reasons.
+         * @param other the other {@link TextChunk}
+         * @return the number of spaces between the end of 'other' and the beginning of this chunk
+         */
+        public float distanceFromEndOf(TextChunk other){
+            return location.distanceFromEndOf(other.location);
+        }
         
         private void printDiagnostics(){
             System.out.println("Text (@" + location.getStartLocation() + " -> " + location.getEndLocation() + "): " + text);
