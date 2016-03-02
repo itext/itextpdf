93b32d3d0369c39df8d7809795bae5929b898727
Ryan Hamer <ryan.hamer@itextpdf.com>
2016-03-02 12:05:43 -0500

    Check to see if either of the roles are null before comparing them.

--- itext/src/main/java/com/itextpdf/text/Phrase.java
+++ itext/src/main/java/com/itextpdf/text/Phrase.java
@@ -47,6 +47,7 @@ package com.itextpdf.text;
 import com.itextpdf.text.Font.FontFamily;
 import com.itextpdf.text.error_messages.MessageLocalization;
 import com.itextpdf.text.pdf.HyphenationEvent;
+import com.itextpdf.text.pdf.PdfName;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -405,7 +406,15 @@ public class Phrase extends ArrayList<Element> implements TextElementArray {
         if (size() > 0 && !chunk.hasAttributes()) {
             try {
                 Chunk previous = (Chunk) get(size() - 1);
-                if (previous.getRole().equals(chunk.getRole()) && !previous.hasAttributes() && !chunk.hasAccessibleAttributes() && !previous.hasAccessibleAttributes()
+                PdfName previousRole = previous.getRole();
+                PdfName chunkRole = chunk.getRole();
+                boolean sameRole;
+                if(previousRole == null || chunkRole == null)
+                	//Set the value to true if either are null since the overwriting of the role will not matter
+                	sameRole = true;
+                else
+                	sameRole = previousRole.equals(chunkRole);
+                if (sameRole && !previous.hasAttributes() && !chunk.hasAccessibleAttributes() && !previous.hasAccessibleAttributes()
                 		&& (f == null
                 		|| f.compareTo(previous.getFont()) == 0)
                 		&& !"".equals(previous.getContent().trim())
