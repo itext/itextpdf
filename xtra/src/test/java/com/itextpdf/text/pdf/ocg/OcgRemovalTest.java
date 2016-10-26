5cbe822d85b35b1e0d2a2fbeeebc9c81c9d79ab1
Lucas Pleß <lucas@xleave.de>
2016-10-26 13:20:59 +0200

    added a test for the OCGParser to demonstrate the missbehavior

--- xtra/src/test/java/com/itextpdf/text/pdf/ocg/OcgRemovalTest.java
+++ xtra/src/test/java/com/itextpdf/text/pdf/ocg/OcgRemovalTest.java
@@ -1,26 +1,25 @@
 package com.itextpdf.text.pdf.ocg;
 
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.pdf.PdfDictionary;
-import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfReader;
-import junit.framework.Assert;
+import com.itextpdf.text.pdf.PdfStamper;
 import org.junit.Test;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class OcgRemovalTest {
 
-    private static final String INPUT = "./src/test/resources/com/itextpdf/text/pdf/ocg/peek-a-boo2.pdf";
+    private static final String INPUT = "./src/test/resources/com/itextpdf/text/pdf/ocg/Example.pdf";
+    private static final String OUTPUT = "./src/test/resources/com/itextpdf/text/pdf/ocg/Example-out.pdf";
 
     @Test
     public void removeOcgLayer() throws IOException, DocumentException {
         PdfReader reader = new PdfReader(INPUT);
         OCGRemover ocgRemover = new OCGRemover();
-        ocgRemover.removeLayers(reader, "Do you see me?");
-        PdfDictionary catalog = reader.getCatalog();
-        Assert.assertNull(catalog.get(PdfName.OCPROPERTIES));
-        Assert.assertNotSame(PdfName.USEOC, catalog.get(PdfName.PAGEMODE));
+        ocgRemover.removeLayers(reader, "ecc.pricebutton");
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(OUTPUT));
+        stamper.close();
         reader.close();
     }
 }
