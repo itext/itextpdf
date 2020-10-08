17843e8bcd620f4a8f2fcf60e21d0e35107798fb
iText Software <development@itextpdf.com>
2020-10-08 18:08:57 +0300

    [RELEASE] 2.0.8-SNAPSHOT -> 2.0.8

--- itext/src/main/java/com/itextpdf/text/Version.java
+++ itext/src/main/java/com/itextpdf/text/Version.java
@@ -76,7 +76,7 @@ public final class Version {
 	 * This String contains the version number of this iText release.
 	 * For debugging purposes, we request you NOT to change this constant.
 	 */
-    private final String release = "2.0.8-SNAPSHOT";
+    private final String release = "2.0.8";
 	/**
 	 * This String contains the iText version as shown in the producer line.
 	 * iText is a product developed by iText Group NV.
